54192824edf0b97290bceb8f846f73ae225cdc90
KazusaNakagawa <kazusanakagawa@gmail.com>
2021-03-21 21:45:57 +0900

    bundle exec rails db:migrate:resetを実行

--- rails_app/app/models/store.rb
+++ rails_app/app/models/store.rb
@@ -5,22 +5,31 @@
 # Table name: stores
 #
 #  id                     :bigint           not null, primary key
+#  address                :string(255)
 #  allow_password_change  :boolean          default(FALSE)
 #  confirmation_sent_at   :datetime
 #  confirmation_token     :string(255)
 #  confirmed_at           :datetime
 #  email                  :string(255)
 #  encrypted_password     :string(255)      default(""), not null
-#  image                  :string(255)
+#  fax                    :string(255)
+#  furigana               :string(255)
+#  genre                  :string(255)
 #  name                   :string(255)
-#  nickname               :string(255)
+#  other                  :string(255)
+#  postal_code            :string(255)
 #  provider               :string(255)      default("email"), not null
 #  remember_created_at    :datetime
 #  reset_password_sent_at :datetime
 #  reset_password_token   :string(255)
+#  responsible_party      :string(255)
+#  restaurant             :string(255)
+#  seat                   :string(255)
+#  tel                    :string(255)
 #  tokens                 :text(65535)
 #  uid                    :string(255)      default(""), not null
 #  unconfirmed_email      :string(255)
+#  url                    :string(255)
 #  created_at             :datetime         not null
 #  updated_at             :datetime         not null
 #
