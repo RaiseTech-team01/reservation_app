9a91de8e0b5e204989db2b754b9741c60a6a8cb4
KazusaNakagawa <kazusanakagawa@gmail.com>
2021-03-26 22:58:38 +0900

    Errorしてます

--- rails_app/app/controllers/api/v1/auth/registrations_controller.rb
+++ rails_app/app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,2 +1,16 @@
 class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
+  skip_before_action :verify_authenticity_token
+
+  private
+
+    # binding.pry
+    def sign_up_params
+      binding.pry
+      params.permit(:name, :email, :furigana, :tel, :birthday, :password, :password_confirmation)
+    end
+
+    def account_update_params
+      binding.pry
+      params.permit(:name, :email, :furigana, :tel, :birthday)
+    end
 end
