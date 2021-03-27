9f10969d07fe3f07dc8c7006ac549fc05edd7107
KazusaNakagawa <kazusanakagawa@gmail.com>
2021-03-27 13:58:24 +0900

    user登録機能を実装

--- rails_app/app/controllers/api/v1/auth/registrations_controller.rb
+++ rails_app/app/controllers/api/v1/auth/registrations_controller.rb
@@ -3,14 +3,11 @@ class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsCon
 
   private
 
-    # binding.pry
     def sign_up_params
-      binding.pry
-      params.permit(:name, :email, :furigana, :tel, :birthday, :password, :password_confirmation)
+      params.permit(:name, :email, :furigana, :tel, :birthday, :six, :address, :password, :password_confirmation)
     end
 
     def account_update_params
-      binding.pry
-      params.permit(:name, :email, :furigana, :tel, :birthday)
+      params.permit(:name, :email, :furigana, :tel, :birthday, :six, :address)
     end
 end
