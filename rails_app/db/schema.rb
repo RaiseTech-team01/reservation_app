9e9508a289af5cd75854aa946f4bbcb5839d6edc
KazusaNakagawa <kazusanakagawa@gmail.com>
2021-03-30 00:24:36 +0900

    crate payment model & migrate

--- rails_app/db/schema.rb
+++ rails_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_18_121944) do
+ActiveRecord::Schema.define(version: 2021_03_28_133435) do
 
   create_table "calendars", charset: "utf8mb4", force: :cascade do |t|
     t.string "business_hours"
@@ -31,6 +31,11 @@ ActiveRecord::Schema.define(version: 2021_03_18_121944) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["store_id"], name: "index_deliveries_on_store_id"
     t.index ["user_id"], name: "index_deliveries_on_user_id"
+  end
+
+  create_table "payments", charset: "utf8mb4", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "question_answers", charset: "utf8mb4", force: :cascade do |t|
