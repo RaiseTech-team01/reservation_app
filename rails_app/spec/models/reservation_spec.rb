f7e1b0ded66f0a7c29ecab70869383e31649c12c
KazusaNakagawa <kazusanakagawa@gmail.com>
2021-03-30 08:44:12 +0900

    正常系テストを実装 reservation model

--- rails_app/spec/models/reservation_spec.rb
+++ rails_app/spec/models/reservation_spec.rb
@@ -28,5 +28,11 @@
 require "rails_helper"
 
 RSpec.describe Reservation, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "必要な情報が揃っている場合" do
+    let(:reservation) { build(:reservation) }
+
+    it "予約登録ができる" do
+      expect(reservation).to be_valid
+    end
+  end
 end
