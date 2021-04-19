413b9e32e86ec51a9d1d235ec8ecbc8f29f72781
KazusaNakagawa <kazusanakagawa@gmail.com>
2021-04-19 23:26:10 +0900

    routes の修正
    (cherry picked from commit 666f96cc30a9e87fb2a7b67136140be778b74b9e)

--- rails_app/config/routes.rb
+++ rails_app/config/routes.rb
@@ -8,15 +8,15 @@ Rails.application.routes.draw do
         registrations: "api/v1/auth/registrations",
         sessions: "api/v1/auth/sessions",
       }
-    end
-  end
 
-  namespace :api do
-    namespace :v1 do
       mount_devise_token_auth_for "Store", at: "store_auth", controllers: {
         registrations: "api/v1/store_auth/registrations",
         sessions: "api/v1/store_auth/sessions",
       }
+
+      namespace :reversion do
+        resources :registrations, only: [:create, :destroy]
+      end
     end
   end
 end
