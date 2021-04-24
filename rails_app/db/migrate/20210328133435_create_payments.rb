9279f463f6c8484b062a8e32daf990ed6647db95
KazusaNakagawa <kazusanakagawa@gmail.com>
2021-04-24 11:23:46 +0900

    [fix] rubocop
    (cherry picked from commit de8c4363cd16bc49feb50357714eadfed86b43a1)

--- rails_app/db/migrate/20210328133435_create_payments.rb
+++ rails_app/db/migrate/20210328133435_create_payments.rb
@@ -1,7 +1,5 @@
 class CreatePayments < ActiveRecord::Migration[6.1]
   def change
-    create_table :payments do |t|
-      t.timestamps
-    end
+    create_table :payments, &:timestamps
   end
 end
--- rails_app/spec/requests/api/v1/base_api_spec.rb
+++ rails_app/spec/requests/api/v1/base_api_spec.rb
@@ -2,5 +2,6 @@ require "rails_helper"
 
 RSpec.describe "Api::V1::BaseApis", type: :request do
   describe "GET /index" do
+    # ...
   end
 end
