1d00e62b52dafaf359ed0552dd479959cb9e8401
KazusaNakagawa <kazusanakagawa@gmail.com>
2021-04-25 11:45:10 +0900

    [imp] 指定店舗の予約一覧表示

--- rails_app/app/serializers/api/v1/reservation_index_serializer.rb
+++ rails_app/app/serializers/api/v1/reservation_index_serializer.rb
@@ -1,3 +1,5 @@
 class Api::V1::ReservationIndexSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :date_at, :date_on
+  belongs_to :store, serializer: Api::V1::StoreSerializer
+  belongs_to :user, serializer: Api::V1::UserSerializer
 end
