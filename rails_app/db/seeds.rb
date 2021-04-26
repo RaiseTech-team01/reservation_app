39da44b1edc2ca6fedd2319f3a9ae8e9359b09c4
KazusaNakagawa <kazusanakagawa@gmail.com>
2021-04-26 19:48:51 +0900

    [imp] User, Store, Reservation のseedを設定

--- rails_app/db/seeds.rb
+++ rails_app/db/seeds.rb
@@ -1,7 +1,37 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+(1..3).each do |i| # rubocop:disable Metrics/BlockLength
+  User.create!(name: "ユーザー#{i}",
+               email: "user#{i}@sample.com",
+               furigana: "ユーザー#{i}",
+               tel: "12345678#{i}",
+               birthday: "2021041#{i}",
+               six: "男",
+               address: "大阪",
+               password: "#{i}#{i}#{i}#{i}#{i}#{i}",
+               password_confirmation: "#{i}#{i}#{i}#{i}#{i}#{i}")
+
+  Store.create!(name: "店舗#{i}",
+                email: "store#{i}@sample.com",
+                furigana: "テンポ#{i}",
+                tel: "12345678#{i}",
+                fax: "12345678#{i}",
+                postal_code: "1234567",
+                url: "http://sample#{i}",
+                address: "大阪",
+                seat: "#{i}#{i}#{i}",
+                restaurant: "店舗名#{i}",
+                genre: "飲食業",
+                responsible_party: "担当者#{i}",
+                other: "アレルギー#{i}",
+                password: "#{i}#{i}#{i}#{i}#{i}#{i}",
+                password_confirmation: "#{i}#{i}#{i}#{i}#{i}#{i}")
+
+  Reservation.create!(date_at: "2021-0#{i}-0#{i}",
+                      date_on: "2021-0#{i}-0#{i}",
+                      number_people: "#{i}#{i}#{i}",
+                      menu: "メニュー#{i}",
+                      budget: 10000,
+                      inquiry: "メモ#{i}",
+                      reservation_number: "ABC#{i}#{i}#{i}" * 2,
+                      user_id: i,
+                      store_id: i)
+end
