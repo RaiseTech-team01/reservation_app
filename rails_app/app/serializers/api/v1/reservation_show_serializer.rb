15544b47b809d6a9b28548335ab22f6f89b3cb1b
KazusaNakagawa <kazusanakagawa@gmail.com>
2021-04-27 02:02:51 +0900

    [imp] show serializers の設定
    (cherry picked from commit 30a7c1737beaadfb286e2d13233fc5dd4b83f942)

--- rails_app/app/serializers/api/v1/reservation_show_serializer.rb
+++ rails_app/app/serializers/api/v1/reservation_show_serializer.rb
@@ -1,3 +1,5 @@
 class Api::V1::ReservationShowSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :date_at, :date_on, :number_people, :menu, :budget, :inquiry, :reservation_number
+  belongs_to :store, serializer: Api::V1::StoreSerializer
+  belongs_to :user, serializer: Api::V1::UserSerializer
 end
