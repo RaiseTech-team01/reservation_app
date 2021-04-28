ce4d089af1975305a407a3b1e41dbc6e13ca0f58
KazusaNakagawa <kazusanakagawa@gmail.com>
2021-04-28 21:33:12 +0900

    [mod] TODOコメント追記
    (cherry picked from commit e813e682d9e851a4e3032daedf0bcafd8df60b51)

--- rails_app/spec/requests/home_spec.rb
+++ rails_app/spec/requests/home_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 RSpec.describe "Homes", type: :request do
   describe "GET /top" do
     it "returns http success" do
+      # TODO: テスト実装は後回し 2021/4/28: kz-naka
       # get "/home/top"
       # expect(response).to have_http_status(:success)
     end
