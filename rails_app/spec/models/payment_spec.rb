d81c866bdaea6f020b8c3c0cda8d60666e98c945
KazusaNakagawa <kazusanakagawa@gmail.com>
2021-05-02 20:23:17 +0900

    [mod] db:migrateの実行

--- rails_app/spec/models/payment_spec.rb
+++ rails_app/spec/models/payment_spec.rb
@@ -2,9 +2,18 @@
 #
 # Table name: payments
 #
-#  id         :bigint           not null, primary key
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
+#  id             :bigint           not null, primary key
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  reservation_id :bigint
+#
+# Indexes
+#
+#  index_payments_on_reservation_id  (reservation_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (reservation_id => reservations.id)
 #
 require "rails_helper"
 
