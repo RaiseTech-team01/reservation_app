54218c058dbea88872b4155b74a2d6771f5b1340
KazusaNakagawa <kazusanakagawa@gmail.com>
2021-05-03 12:47:03 +0900

    [imp] バリテーションのテスト

--- rails_app/spec/models/reservation_spec.rb
+++ rails_app/spec/models/reservation_spec.rb
@@ -30,12 +30,64 @@
 #
 require "rails_helper"
 
-RSpec.describe Reservation, type: :model do
-  context "必要な情報が揃っている場合" do
-    let(:reservation) { build(:reservation) }
+RSpec.describe Reservation, type: :model do # rubocop:disable Metrics/BlockLength
+  describe "正常系 バリデーションのテスト" do
+    context "必要な情報が揃っている場合" do
+      let(:reservation) { build(:reservation) }
 
-    it "予約登録ができる" do
-      expect(reservation).to be_valid
+      it "予約登録ができる" do
+        expect(reservation).to be_valid
+      end
+    end
+  end
+
+  describe "異常系 バリデーションのテスト" do # rubocop:disable Metrics/BlockLength
+    context "予約日が入力されていない時" do
+      let(:reservation) { build(:reservation, date_at: nil) }
+
+      it "予約登録ができない" do
+        expect(reservation).not_to be_valid
+      end
+    end
+
+    context "予約時間が入力されていない時" do
+      let(:reservation) { build(:reservation, date_on: nil) }
+
+      it "予約登録ができない" do
+        expect(reservation).not_to be_valid
+      end
+    end
+
+    context "予約人数が入力されていない時" do
+      let(:reservation) { build(:reservation, number_people: nil) }
+
+      it "予約登録ができない" do
+        expect(reservation).not_to be_valid
+      end
+    end
+
+    context "メニューが入力されていない時" do
+      let(:reservation) { build(:reservation, menu: nil) }
+
+      it "予約登録ができない" do
+        expect(reservation).not_to be_valid
+      end
+    end
+
+    context "予算が入力されていない時" do
+      let(:reservation) { build(:reservation, budget: nil) }
+
+      it "予約登録ができない" do
+        expect(reservation).not_to be_valid
+      end
+    end
+
+    context "予約番号が入力されていない時" do
+      let(:reservation) { build(:reservation, reservation_number: nil) }
+
+      it "予約登録ができない" do
+        expect(reservation).not_to be_valid
+      end
     end
   end
 end
