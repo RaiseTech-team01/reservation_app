64b10e1878b5ffc9ba5402917f941fa5fad2147f
KazusaNakagawa <kazusanakagawa@gmail.com>
2021-05-08 17:58:04 +0900

    [mod] :store_id に修正

--- rails_app/app/controllers/api/v1/reservations_controller.rb
+++ rails_app/app/controllers/api/v1/reservations_controller.rb
@@ -4,13 +4,13 @@ class Api::V1::ReservationsController < Api::V1::BaseApiController
 
   def index
     # 指定店舗の一覧を表示
-    reservations = current_user.reservations.where(params["store_id"])
+    reservations = current_user.reservations.where(params[:store_id])
     render json: reservations, each_serializer: Api::V1::ReservationSerializer
   end
 
   def show
     # 指定店舗の選択した予約詳細を表示
-    reservations = current_user.reservations.where(params["store_id"])
+    reservations = current_user.reservations.where(params[:store_id])
     reservation = reservations.find(params[:id])
     render json: reservation, serializer: Api::V1::ReservationSerializer
   end
@@ -20,7 +20,7 @@ class Api::V1::ReservationsController < Api::V1::BaseApiController
     # 生成した予約番号を格納
     reservation.reservation_number = reservation.create_reservation_num
     # 指定店舗があることを確認し格納
-    reservation.store_id = Store.find(params["store_id"]).id
+    reservation.store_id = Store.find(params[:store_id]).id
     reservation.save!
 
     render json: reservation, serializer: Api::V1::ReservationSerializer
@@ -28,7 +28,7 @@ class Api::V1::ReservationsController < Api::V1::BaseApiController
 
   def update
     # 対象の予約を検索する
-    reservations = current_user.reservations.where(params["store_id"])
+    reservations = current_user.reservations.where(params[:store_id])
     reservation = reservations.find(params[:id])
     # リクエストで変更のある値を更新
     reservation.update!(reservation_params)
