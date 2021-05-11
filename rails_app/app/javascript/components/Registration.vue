8f267b19b0a952052f4b8d0d78bc20434cfbff6d
Yuki.Nishimura <yukinkopen@gmail.com>
2021-05-11 13:37:31 +0900

    [mod] アカウント登録画面のHTMLコードを実装

--- rails_app/app/javascript/components/Registration.vue
+++ rails_app/app/javascript/components/Registration.vue
@@ -14,42 +14,61 @@
               <a class="font-bold" href="index.html">トップ</a>
               <span> > </span>
               <a class="font-bold" href="index.html">ログイン</a>
+              <span> > </span>
+              <a class="font-bold" href="index.html">新規登録入力</a>
             </h3>
           </div>
           <div>
-            <h2 class="mt-10 text-3xl text-center text-blue-800 font-semibold">予約サービス ログインページ</h2>
+            <div>
+              <p>
+                <span>入力</span>
+                <span>確認</span>
+                <span>登録</span>
+              </p>
+            </div>
           </div>
           <div>
-            <form class="mt-10 ml-10">
-              <table>
-                <tbody>
-                  <tr>
-                    <th class="pr-3 pb-10 text-2xl text-blue-800 text-right border-none">ID</th>
-                    <td class="border-none pb-10">
-                      <input data-v-6fb8108a="" name="email" type="email" class="w-80 h-10 border-blue-800 border-2" autocomplete="email">
-                    </td>
-                  </tr>
-                  <tr>
-                    <th class="pr-3 text-2xl text-blue-800 text-right border-none">パスワード</th>
-                    <td class="border-none">
-                      <input data-v-6fb8108a="" name="password" type="password" class="w-80 h-10 border-blue-800 border-2" autocomplete="on">
-                    </td>
-                  </tr>
-                </tbody>
-              </table>
-              <div id="login_checkbox" class="mt-10 text-center">
-                <input id="is_auto_login" class="w-20 border-blue-800 border-2" name="is_auto_login" type="checkbox">
-                <label class="text-xl text-blue-800" for="is_auto_login">次からは自動でログインする</label><br>
-              </div>
-              <div class="mt-10 text-center">
-                <input class="w-40 h-12 mr-8 p-1 text-2xl text-blue-800 bg-yellow-400" type="submit" :loading="loading" @click="submit" value="ログイン">
-                <input class="w-40 h-12 p-1 text-2xl text-blue-800 bg-yellow-400" type="button" value="戻る">
-              </div>
-          </form>
-          <div class="text-center mt-10 text-blue-800">
-            <p class="mx-auto w-96 text-xl break-words text-left">アカウント未登録の方は下記から登録してください。</p>
-            <input class="mt-4 w-80 h-12 p-1 text-2xl text-blue-800 bg-yellow-400" type="button" value="新規登録">
-          </div>
+            <p>お客様の情報を入力してください</p>
+            <form>
+            <table>
+              <tr>
+                <td>ご要望のID</td>
+                <td><input type="text"></td>
+              </tr>
+              <tr>
+                <td>氏名</td>
+                <td><input type="text"></td>
+              </tr>
+              <tr>
+                <td>カナ</td>
+                <td><input type="text"></td>
+              </tr>
+              <tr>
+                <td>年齢</td>
+                <td><input type="text"></td>
+              </tr>
+              <tr>
+                <td>性別</td>
+                <td><input type="text"></td>
+              </tr>
+              <tr>
+                <td>住所</td>
+                <td><input type="text"></td>
+              </tr>
+              <tr>
+                <td>メールアドレス</td>
+                <td><input type="email"></td>
+              </tr>
+              <tr>
+                <td>電話番号</td>
+                <td><input type="tel"></td>
+              </tr>
+            </table>
+            <div>
+              <input type="button" value="登　録">
+              <input type="button" value="戻　る">
+            </div>
+            </form>
           </div>
         </div>
       </div>
@@ -61,7 +80,6 @@
 </template>
 
 <script>
-import axios from 'axios'
 import Router from "../router/router";
 import Header from "./layout/Header.vue"
 import Navigation from "./layout/Navigation.vue"
@@ -70,10 +88,6 @@ import Footer from "./layout/Footer.vue"
 export default {
   data: function () {
     return {
-      name:  '',
-      loading: false,
-      email: '',
-      password: '',
     }
   },
 
@@ -84,41 +98,6 @@ export default {
   },
 
   methods: {
-    async submit() {
-      this.loading = true
-      const params = {
-        email: this.email,
-        password: this.password,
-      }
-      await axios
-        .post("/api/v1/auth/sign_in", params)
-        .then(response => {
-          localStorage.setItem("access-token", response.headers["access-token"]);
-          localStorage.setItem("uid", response.headers["uid"]);
-          localStorage.setItem("client", response.headers["client"]);
-
-          Router.push("/");
-
-          // TODO: Vuex でログイン状態を管理するようになったら消す
-          window.location.reload();
-        })
-        .catch(e => {
-          // TODO: 適切な Error 表示
-          if (e.response) {
-            console.log(e.response.data);
-            console.log(e.response.status);
-            console.log(e.response.headers);
-          } else if (error.request) {
-            console.log(e.request);
-          } else {
-            console.log('Error', e.message);
-          }
-        })
-        .finally(() => {
-          this.loading = false
-        })
-        console.log("hi")
-    }
   }
 }
 </script>
@@ -127,37 +106,5 @@ export default {
 p {
   font-size: 2em;
   text-align: center;
-}
-
-/* ラベルのスタイル　*/
-#login_checkbox label {
-  padding-left: 38px; /* ラベルの位置 */
-  line-height: 32px;
-  display: inline-block;
-  cursor: pointer;
-  position: relative;
-}
-
-/* ボックスのスタイル */
-#login_checkbox label:before {
-  content: '';
-  width: 32px; /* ボックスの横幅 */
-  height: 32px; /* ボックスの縦幅 */
-  display: inline-block;
-  position: absolute;
-  left: 0;
-  background-color: #fff;
-  border: 2px solid #1E40AF;
-}
-/* 元のチェックボックスを表示しない */
-#login_checkbox input[type=checkbox] {
-  display: none;
-}
-/* チェックした時のスタイル */
-#login_checkbox input[type=checkbox]:checked + label:before {
-  content: '\2713'; /* チェックの文字 */ 
-  font-size: 24px; /* チェックのサイズ */
-  color: #fff; /* チェックの色 */
-  background-color: #06f; /* チェックした時の色 */
 }
 </style>
