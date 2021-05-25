10786d0dded87e9c9c30c190bf423999bd62783e
Nishimura <yukinkopen@gmail.com>
2021-05-26 02:57:23 +0900

    [mod] Registration.vue の内容を複写

--- rails_app/app/javascript/components/RegistrationConfirm.vue
+++ rails_app/app/javascript/components/RegistrationConfirm.vue
@@ -1,82 +1,95 @@
 <template>
-  <div class="main m-0">
-    <dir class="header m-0 text-center">
-      <Header/>
-    </dir>
-    <main class="mt-28"><!-- min-widthを設定する -->
-      <dir class="navigation m-0">
-        <Navigation/>
-      </dir>
-      <div class="flex justify-center h-screen">
-        <div class="bg-gray-300" style="width: 500px">
+<div class="main m-0">
+  <dir class="header m-0 text-center pl-0">
+    <Header />
+  </dir>
+  <main>
+    <div class="flex justify-center">
+      <div class="bg-gray-300" style="width: 766px">
+        <div>
+          <h3 class="mt-10 ml-4 text-xl text-blue-800">
+            <a class="font-bold hover:text-blue-500" href="index.html">トップ</a>
+            <span> > </span>
+            <a class="font-bold hover:text-blue-500" href="index.html">ログイン</a>
+            <span> > </span>
+            <a class="font-bold hover:text-blue-500" href="index.html">新規登録入力</a>
+          </h3>
+        </div>
+        <div class="mt-16">
           <div>
-            <h3 class="mt-4 ml-4 text-xl text-blue-800">
-              <a class="font-bold" href="index.html">トップ</a>
-              <span> > </span>
-              <a class="font-bold" href="index.html">ログイン</a>
-              <span> > </span>
-              <a class="font-bold" href="index.html">新規登録確認</a>
-            </h3>
-          </div>
-          <div>
-            <div>
-              <p>
-                <span>入力</span>
-                <span>確認</span>
-                <span>登録</span>
-              </p>
-            </div>
-          </div>
-          <div>
-            <p>下記の情報を登録して良いですか？</p>
-            <form>
-            <table>
-              <tr>
-                <td>ID</td>
-                <td></td>
-              </tr>
-              <tr>
-                <td>氏名</td>
-                <td></td>
-              </tr>
-              <tr>
-                <td>カナ</td>
-                <td></td>
-              </tr>
-              <tr>
-                <td>メールアドレス</td>
-                <td></td>
-              </tr>
-              <tr>
-                <td>電話番号</td>
-                <td></td>
-              </tr>
-              <tr>
-                <td>年齢</td>
-                <td></td>
-              </tr>
-              <tr>
-                <td>性別</td>
-                <td></td>
-              </tr>
-              <tr>
-                <td>住所</td>
-                <td></td>
-              </tr>
-            </table>
-            <div>
-              <input type="button" value="登録完了">
-              <input type="button" value="戻　る">
-            </div>
-            </form>
+            <p class="whitespace-nowrap flex justify-around md:justify-center md:space-x-12 md:transform md:scale-125 md:flex-none">
+              <span class="arrow-block">入力</span>
+              <span class="arrow-block-inactive">確認</span>
+              <span class="arrow-block-inactive">登録</span>
+            </p>
           </div>
         </div>
+        <div>
+          <h2 class="mt-16 mb-8 font-bold text-3xl md:text-4xl text-center text-blue-800">お客様の情報を入力してください</h2>
+          <form>
+            <table class="m-2 md:m-10 table-auto">
+              <tr>
+                <td class="text-3xl mg:text-4xl whitespace-nowrap form-table-padding p-4 md:p-6 text-blue-800">氏名</td>
+                <td class="space-x-4">
+                  <div class="flex justify-between space-x-2 md:flex-none">
+                    <input class="w-1/2 md:w-44 h-12 md:mr-4 border-2 md:border-4 border-blue-700 bg-gray-100 pl-4 text-3xl" type="text" />
+                    <input class="w-1/2 md:w-44 h-12 md:ml-4 border-2 md:border-4 border-blue-700 bg-gray-100 pl-4 text-3xl" type="text" />
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td class="text-3xl mg:text-4xl whitespace-nowrap form-table-padding p-4 md:p-6 text-blue-800">カナ</td>
+                <td>
+                  <div class="flex justify-between space-x-2 md:flex-none">
+                    <input class="w-1/2 md:w-44 h-12 md:mr-4 border-2 md:border-4 border-blue-700 bg-gray-100 pl-4 text-3xl" type="text" />
+                    <input class="w-1/2 md:w-44 h-12 md:ml-4 border-2 md:border-4 border-blue-700 bg-gray-100 pl-4 text-3xl" type="text" />
+                  </div>
+                </td>
+              </tr>
+              <tr>
+                <td class="text-2xl md:text-3xl whitespace-nowrap form-table-padding p-4 md:p-6 text-blue-800">メール<br class="md:hidden" />アドレス</td>
+                <td>
+                  <input class="w-full md:w-96 h-12 border-2 md:border-4 border-blue-700 bg-gray-100 pl-4 text-3xl" type="email" />
+                </td>
+              </tr>
+              <tr>
+                <td class="text-2xl md:text-4xl whitespace-nowrap form-table-padding p-4 md:p-6 text-blue-800">電話<br class="md:hidden" />番号</td>
+                <td>
+                  <input class="w-full md:w-96 h-12 border-2 md:border-4 border-blue-700 bg-gray-100 pl-4 text-3xl" type="tel" />
+                </td>
+              </tr>
+              <tr>
+                <td class="text-3xl md:text-4xl whitespace-nowrap form-table-padding p-4 md:p-6 text-blue-800">年齢</td>
+                <td>
+                  <input class="w-1/2 md:w-44 h-12 border-2 md:border-4 border-blue-700 bg-gray-100 pl-4 text-3xl" type="text" />
+                </td>
+              </tr>
+              <tr>
+                <td class="text-3xl md:text-4xl whitespace-nowrap form-table-padding p-4 md:p-6 text-blue-800">性別</td>
+                <td>
+                  <input class="w-1/2 md:w-44 h-12 border-2 md:border-4 border-blue-700 bg-gray-100 pl-4 text-3xl" type="text" />
+                </td>
+              </tr>
+              <tr>
+                <td class="text-3xl md:text-4xl whitespace-nowrap form-table-padding p-4 md:p-6 text-blue-800">住所</td>
+                <td>
+                  <input class="w-full md:w-96 h-12 border-2 md:border-4 border-blue-700 bg-gray-100 pl-4 text-3xl" type="text" />
+                </td>
+              </tr>
+            </table>
+            <div class="text-center space-x-4 md:space-x-8 mt-14 mb-28">
+              <input class="inline-block px-10 md:px-16 py-2 rounded-xl font-bold bg-yellow-300 text-4xl text-blue-800 cursor-pointer hover:bg-yellow-200 hover:text-blue-600 active:bg-red-200" type="button" value="登　録" />
+              <input class="inline-block px-10 md:px-16 py-2 rounded-xl font-bold bg-yellow-300 text-4xl text-blue-800 cursor-pointer hover:bg-yellow-200 hover:text-blue-600 active:bg-red-200" type="button" value="戻　る" />
+            </div>
+          </form>
+        </div>
       </div>
-      <dir class="footer">
-        <Footer/>
-      </dir>
-    </main>
-  </div>
+    </div>
+    <dir class="footer m-0 pl-0">
+      <Footer />
+    </dir>
+  </main>
+</div>
 </template>
 
 <script>
