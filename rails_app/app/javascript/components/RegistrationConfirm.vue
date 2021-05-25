4f864b6ca232b47b6d345379a794bad2a6c4a485
Nishimura <yukinkopen@gmail.com>
2021-05-26 05:25:46 +0900

    [mod] アカウント登録確認画面向けにレイアウトを修正   - テーブルの中身を文字列に変更   - テーブルの種別と値の文字列の高さが揃うように、セルの高さを設定

--- rails_app/app/javascript/components/RegistrationConfirm.vue
+++ rails_app/app/javascript/components/RegistrationConfirm.vue
@@ -12,73 +12,85 @@
             <span> > </span>
             <a class="font-bold hover:text-blue-500" href="index.html">ログイン</a>
             <span> > </span>
-            <a class="font-bold hover:text-blue-500" href="index.html">新規登録入力</a>
+            <a class="font-bold hover:text-blue-500" href="index.html">新規登録確認</a>
           </h3>
         </div>
         <div class="mt-16">
           <div>
             <p class="whitespace-nowrap flex justify-around md:justify-center md:space-x-12 md:transform md:scale-125 md:flex-none">
-              <span class="arrow-block">入力</span>
-              <span class="arrow-block-inactive">確認</span>
+              <span class="arrow-block-inactive">入力</span>
+              <span class="arrow-block">確認</span>
               <span class="arrow-block-inactive">登録</span>
             </p>
           </div>
         </div>
         <div>
-          <h2 class="mt-16 mb-8 font-bold text-3xl md:text-4xl text-center text-blue-800">お客様の情報を入力してください</h2>
+          <h2 class="mt-16 mb-8 font-bold text-3xl md:text-4xl text-center text-blue-800">下記の情報を登録して良いですか？</h2>
           <form>
-            <table class="m-2 md:m-10 table-auto">
-              <tr>
-                <td class="text-3xl mg:text-4xl whitespace-nowrap form-table-padding p-4 md:p-6 text-blue-800">氏名</td>
+            <table class="m-2 md:m-10 table-auto max-w-full">
+              <tr class="h-24">
+                <td class="text-3xl mg:text-4xl whitespace-nowrap form-table-padding pl-4 md:pl-6 text-blue-800">氏名</td>
                 <td class="space-x-4">
-                  <div class="flex justify-between space-x-2 md:flex-none">
-                    <input class="w-1/2 md:w-44 h-12 md:mr-4 border-2 md:border-4 border-blue-700 bg-gray-100 pl-4 text-3xl" type="text" />
-                    <input class="w-1/2 md:w-44 h-12 md:ml-4 border-2 md:border-4 border-blue-700 bg-gray-100 pl-4 text-3xl" type="text" />
+                  <div>
+                    <p class="w-full md:w-96 md:mr-4 pl-4 text-3xl text-blue-800 font-bold">
+                      田中　一郎
+                    </p>
                   </div>
                 </td>
               </tr>
-              <tr>
-                <td class="text-3xl mg:text-4xl whitespace-nowrap form-table-padding p-4 md:p-6 text-blue-800">カナ</td>
+              <tr class="h-24">
+                <td class="text-3xl mg:text-4xl whitespace-nowrap form-table-padding pl-4 md:pl-6 text-blue-800">カナ</td>
                 <td>
-                  <div class="flex justify-between space-x-2 md:flex-none">
-                    <input class="w-1/2 md:w-44 h-12 md:mr-4 border-2 md:border-4 border-blue-700 bg-gray-100 pl-4 text-3xl" type="text" />
-                    <input class="w-1/2 md:w-44 h-12 md:ml-4 border-2 md:border-4 border-blue-700 bg-gray-100 pl-4 text-3xl" type="text" />
+                  <div>
+                    <p class="w-full md:w-96 md:mr-4 pl-4 text-3xl text-blue-800 font-bold">
+                      タナカ　イチロウ
+                    </p>
                   </div>
                 </td>
               </tr>
-              <tr>
-                <td class="text-2xl md:text-3xl whitespace-nowrap form-table-padding p-4 md:p-6 text-blue-800">メール<br class="md:hidden" />アドレス</td>
+              <tr class="h-24">
+                <td class="text-2xl md:text-3xl whitespace-nowrap form-table-padding pl-4 md:pl-6 text-blue-800">メール<br class="md:hidden" />アドレス</td>
                 <td>
-                  <input class="w-full md:w-96 h-12 border-2 md:border-4 border-blue-700 bg-gray-100 pl-4 text-3xl" type="email" />
+                  <p class="w-full md:w-96 md:mr-4 pl-4 text-3xl text-blue-800 font-bold white break-all">
+                    あああ
+                  </p>
                 </td>
               </tr>
-              <tr>
-                <td class="text-2xl md:text-4xl whitespace-nowrap form-table-padding p-4 md:p-6 text-blue-800">電話<br class="md:hidden" />番号</td>
+              <tr class="h-24">
+                <td class="text-2xl md:text-4xl whitespace-nowrap form-table-padding pl-4 md:pl-6 text-blue-800">電話<br class="md:hidden" />番号</td>
                 <td>
-                  <input class="w-full md:w-96 h-12 border-2 md:border-4 border-blue-700 bg-gray-100 pl-4 text-3xl" type="tel" />
+                  <p class="w-full md:w-96 md:mr-4 pl-4 text-3xl text-blue-800 font-bold">
+                    080-1111-2222
+                  </p>
                 </td>
               </tr>
-              <tr>
-                <td class="text-3xl md:text-4xl whitespace-nowrap form-table-padding p-4 md:p-6 text-blue-800">年齢</td>
+              <tr class="h-24">
+                <td class="text-3xl md:text-4xl whitespace-nowrap form-table-padding pl-4 md:pl-6 text-blue-800">年齢</td>
                 <td>
-                  <input class="w-1/2 md:w-44 h-12 border-2 md:border-4 border-blue-700 bg-gray-100 pl-4 text-3xl" type="text" />
+                  <p class="w-full md:w-96 md:mr-4 pl-4 text-3xl text-blue-800 font-bold">
+                    31 歳
+                  </p>
                 </td>
               </tr>
-              <tr>
-                <td class="text-3xl md:text-4xl whitespace-nowrap form-table-padding p-4 md:p-6 text-blue-800">性別</td>
+              <tr class="h-24">
+                <td class="text-3xl md:text-4xl whitespace-nowrap form-table-padding pl-4 md:pl-6 text-blue-800">性別</td>
                 <td>
-                  <input class="w-1/2 md:w-44 h-12 border-2 md:border-4 border-blue-700 bg-gray-100 pl-4 text-3xl" type="text" />
+                  <p class="w-full md:w-96 md:mr-4 pl-4 text-3xl text-blue-800 font-bold">
+                    男性
+                  </p>
                 </td>
               </tr>
-              <tr>
-                <td class="text-3xl md:text-4xl whitespace-nowrap form-table-padding p-4 md:p-6 text-blue-800">住所</td>
+              <tr class="h-24">
+                <td class="text-3xl md:text-4xl whitespace-nowrap form-table-padding pl-4 md:pl-6 text-blue-800">住所</td>
                 <td>
-                  <input class="w-full md:w-96 h-12 border-2 md:border-4 border-blue-700 bg-gray-100 pl-4 text-3xl" type="text" />
+                  <p class="w-full md:w-96 md:mr-4 pl-4 text-3xl text-blue-800 font-bold">
+                    千葉県千葉市美浜区1-1
+                  </p>
                 </td>
               </tr>
             </table>
             <div class="text-center space-x-4 md:space-x-8 mt-14 mb-28">
-              <input class="inline-block px-10 md:px-16 py-2 rounded-xl font-bold bg-yellow-300 text-4xl text-blue-800 cursor-pointer hover:bg-yellow-200 hover:text-blue-600 active:bg-red-200" type="button" value="登　録" />
+              <input class="inline-block px-10 md:px-16 py-2 rounded-xl font-bold bg-yellow-300 text-4xl text-blue-800 cursor-pointer hover:bg-yellow-200 hover:text-blue-600 active:bg-red-200" type="button" value="登録完了" />
               <input class="inline-block px-10 md:px-16 py-2 rounded-xl font-bold bg-yellow-300 text-4xl text-blue-800 cursor-pointer hover:bg-yellow-200 hover:text-blue-600 active:bg-red-200" type="button" value="戻　る" />
             </div>
           </form>
