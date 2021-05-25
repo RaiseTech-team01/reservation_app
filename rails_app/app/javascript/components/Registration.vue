c82339dc3977f94d46af5057c09dde0f894d6523
Yuki.Nishimura <yukinkopen@gmail.com>
2021-05-25 11:53:40 +0900

    [mod] ボタン押下時の表示を定義

--- rails_app/app/javascript/components/Registration.vue
+++ rails_app/app/javascript/components/Registration.vue
@@ -78,8 +78,8 @@
               </tr>
             </table>
             <div class="text-center space-x-8 mt-14">
-              <input class="inline-block px-16 py-2 rounded-xl font-bold bg-yellow-300 text-4xl text-blue-800 cursor-pointer hover:bg-yellow-200 hover:text-blue-600" type="button" value="登　録" />
-              <input class="inline-block px-16 py-2 rounded-xl font-bold bg-yellow-300 text-4xl text-blue-800 cursor-pointer hover:bg-yellow-200 hover:text-blue-600" type="button" value="戻　る" />
+              <input class="inline-block px-16 py-2 rounded-xl font-bold bg-yellow-300 text-4xl text-blue-800 cursor-pointer hover:bg-yellow-200 hover:text-blue-600 active:bg-red-200" type="button" value="登　録" />
+              <input class="inline-block px-16 py-2 rounded-xl font-bold bg-yellow-300 text-4xl text-blue-800 cursor-pointer hover:bg-yellow-200 hover:text-blue-600 active:bg-red-200" type="button" value="戻　る" />
             </div>
           </form>
         </div>
