2ccf738e3abbdf290c456cc183fc749962c1c83b
Nishimura <yukinkopen@gmail.com>
2021-06-05 17:06:32 +0900

    [mod] 不要なセミコロンを除去

--- rails_app/app/javascript/components/Login.vue
+++ rails_app/app/javascript/components/Login.vue
@@ -96,25 +96,25 @@ export default {
       await axios
         .post("/api/v1/auth/sign_in", params)
         .then(response => {
-          localStorage.setItem("access-token", response.headers["access-token"]);
-          localStorage.setItem("uid", response.headers["uid"]);
-          localStorage.setItem("client", response.headers["client"]);
+          localStorage.setItem("access-token", response.headers["access-token"])
+          localStorage.setItem("uid", response.headers["uid"])
+          localStorage.setItem("client", response.headers["client"])
 
-          Router.push("/");
+          Router.push("/")
 
           // TODO: Vuex でログイン状態を管理するようになったら消す
-          window.location.reload();
+          window.location.reload()
         })
         .catch(e => {
           // TODO: 適切な Error 表示
           if (e.response) {
-            console.log(e.response.data);
-            console.log(e.response.status);
-            console.log(e.response.headers);
+            console.log(e.response.data)
+            console.log(e.response.status)
+            console.log(e.response.headers)
           } else if (error.request) {
-            console.log(e.request);
+            console.log(e.request)
           } else {
-            console.log('Error', e.message);
+            console.log('Error', e.message)
           }
         })
         .finally(() => {
@@ -123,7 +123,7 @@ export default {
         console.log("hi")
     },
     goToRegistration() {
-      Router.push("/sign_up");
+      Router.push("/sign_up")
     }
   }
 }
