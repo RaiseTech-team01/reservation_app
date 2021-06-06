7ecde8f49455eaaf492ee20f9b735bae2bf99ffc
Nishimura <yukinkopen@gmail.com>
2021-06-06 15:04:34 +0900

    [mod] scriptの読み込み処理を共通部分に移動

--- rails_app/app/javascript/app.vue
+++ rails_app/app/javascript/app.vue
@@ -5,3 +5,17 @@
     </div>
   </v-app>
 </template>
+<script>
+export default {
+  mounted() {
+    let scriptEl = document.createElement('script')
+    scriptEl.setAttribute('src', 'https://kit.fontawesome.com/98c77f9d64.js')
+    scriptEl.setAttribute('crossorigin', 'anonymous')
+    document.head.appendChild(scriptEl)
+
+    scriptEl = document.createElement('script')
+    scriptEl.setAttribute('src', 'https://code.jquery.com/jquery-1.10.2.js')
+    document.head.appendChild(scriptEl)
+  }
+}
+</script>
--- rails_app/app/javascript/components/ReservationForm.vue
+++ rails_app/app/javascript/components/ReservationForm.vue
@@ -205,17 +205,6 @@ export default {
 
   methods: {
   },
-
-  mounted() {
-    let scriptEl = document.createElement('script')
-    scriptEl.setAttribute('src', 'https://kit.fontawesome.com/98c77f9d64.js')
-    scriptEl.setAttribute('crossorigin', 'anonymous')
-    document.head.appendChild(scriptEl)
-
-    scriptEl = document.createElement('script')
-    scriptEl.setAttribute('src', 'https://code.jquery.com/jquery-1.10.2.js')
-    document.head.appendChild(scriptEl)
-  }
 }
 </script>
 
