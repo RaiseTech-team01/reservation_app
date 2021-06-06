05e121c78690521534e3df452b50483285476e42
Nishimura <yukinkopen@gmail.com>
2021-06-06 22:10:53 +0900

    [mod] 予約一覧画面のレイアウトに合わせて修正   - テーブルを定義   - 見本となる文字列を設定   - レスポンシブ対応

--- rails_app/app/javascript/components/ReservationList.vue
+++ rails_app/app/javascript/components/ReservationList.vue
@@ -5,7 +5,7 @@
   </dir>
   <main>
     <dir class="navigation hidden md:block m-0 p-0">
-      <Navigation/>
+      <Navigation />
     </dir>
     <div class="flex justify-center">
       <div class="bg-gray-300 info-container">
@@ -13,30 +13,60 @@
           <h3 class="mt-10 ml-4 text-xl text-blue-800">
             <a class="font-bold hover:text-blue-500" href="index.html">トップ</a>
             <span> > </span>
-            <a class="font-bold hover:text-blue-500" href="index.html">予約登録完了</a>
+            <a class="font-bold hover:text-blue-500" href="index.html">予約一覧</a>
           </h3>
         </div>
-        <div class="mt-16">
+        <div class="mt-16 flex flex-col mx-16">
           <div>
-            <p class="whitespace-nowrap flex justify-around md:justify-center md:space-x-12 md:transform md:scale-125 md:flex-none">
-              <span class="arrow-block-inactive">入力</span>
-              <span class="arrow-block-inactive">確認</span>
-              <span class="arrow-block">登録</span>
-            </p>
+            <table class="text-2xl font-bold text-blue-800 mx-4 md:table block">
+              <tr class="border-t-2 border-blue-800 md:table-row block">
+                <td class="pl-2 md:p-2 md:table-cell block">店舗：</td>
+                <td class="pl-2 md:p-2 md:table-cell block">居酒屋A イロハ駅前店</td>
+                <td class="p-2 md:table-cell hidden"><input class="inline-block w-20 py-1 rounded-xl font-bold bg-yellow-300 md:text-xl text-lg text-blue-800 cursor-pointer hover:bg-yellow-200 hover:text-blue-600 active:bg-red-200" type="button" value="詳細"></td>
+              </tr>
+              <tr class="md:table-row block mt-4">
+                <td class="pl-2 md:p-2 md:table-cell block">予約日時：</td>
+                <td class="pl-2 md:p-2 md:table-cell block">2021年3月20日 18:00～</td>
+                <td class="p-2 md:table-cell hidden"><input class="inline-block w-20 py-1 rounded-xl font-bold bg-yellow-300 md:text-xl text-lg text-blue-800 cursor-pointer hover:bg-yellow-200 hover:text-blue-600 active:bg-red-200" type="button" value="変更"></td>
+              </tr>
+              <tr class="md:table-row block mt-4">
+                <td class="pl-2 md:p-2 md:table-cell block"></td>
+                <td class="pl-2 md:p-2 md:table-cell block">人数：3名　<br class="md:hidden" />コース予約：なし</td>
+              </tr>
+            </table>
+            <div class="my-4 md:my-0 text-center space-x-6 md:hidden">
+              <input class="inline-block w-20 py-1 rounded-xl font-bold bg-yellow-300 md:text-xl text-lg text-blue-800 cursor-pointer hover:bg-yellow-200 hover:text-blue-600 active:bg-red-200" type="button" value="詳細">
+              <input class="inline-block w-20 py-1 rounded-xl font-bold bg-yellow-300 md:text-xl text-lg text-blue-800 cursor-pointer hover:bg-yellow-200 hover:text-blue-600 active:bg-red-200" type="button" value="変更">
+            </div>
+          </div>
+          <div>
+            <table class="text-2xl font-bold text-blue-800 mx-4 md:table block">
+              <tr class="border-t-2 border-blue-800 md:table-row block">
+                <td class="pl-2 md:p-2 md:table-cell block">店舗：</td>
+                <td class="pl-2 md:p-2 md:table-cell block">居酒屋B ホヘト駅前店</td>
+                <td class="p-2 md:table-cell hidden"><input class="inline-block w-20 py-1 rounded-xl font-bold bg-yellow-300 md:text-xl text-lg text-blue-800 cursor-pointer hover:bg-yellow-200 hover:text-blue-600 active:bg-red-200" type="button" value="詳細"></td>
+              </tr>
+              <tr class="md:table-row block mt-4">
+                <td class="pl-2 md:p-2 md:table-cell block">予約日時：</td>
+                <td class="pl-2 md:p-2 md:table-cell block">2021年3月19日 17:00～</td>
+                <td class="p-2 md:table-cell hidden"><input class="inline-block w-20 py-1 rounded-xl font-bold bg-yellow-300 md:text-xl text-lg text-blue-800 cursor-pointer hover:bg-yellow-200 hover:text-blue-600 active:bg-red-200" type="button" value="変更"></td>
+              </tr>
+              <tr class="md:table-row block mt-4">
+                <td class="pl-2 md:p-2 md:table-cell block"></td>
+                <td class="pl-2 md:p-2 md:table-cell block">人数：3名　<br class="md:hidden" />コース予約：なし</td>
+              </tr>
+            </table>
+            <div class="my-4 pb-4 md:my-0 text-center space-x-6 md:hidden border-b-2 border-blue-800">
+              <input class="inline-block w-20 py-1 rounded-xl font-bold bg-yellow-300 md:text-xl text-lg text-blue-800 cursor-pointer hover:bg-yellow-200 hover:text-blue-600 active:bg-red-200" type="button" value="詳細">
+              <input class="inline-block w-20 py-1 rounded-xl font-bold bg-yellow-300 md:text-xl text-lg text-blue-800 cursor-pointer hover:bg-yellow-200 hover:text-blue-600 active:bg-red-200" type="button" value="変更">
+            </div>
           </div>
         </div>
-        <div>
-          <h2 class="mt-16 mb-8 font-bold text-3xl md:text-4xl text-center text-blue-800">ご予約を完了いたしました。</h2>
-          <p class="text-center md:space-x-10 space-x-3">
-            <span class="mt-16 mb-8 font-bold text-2xl md:text-3xl text-center text-blue-800">予約番号</span>
-            <span class="mt-16 mb-8 font-bold text-2xl md:text-3xl text-center text-blue-800">TNX001122</span>
-          </p>
-          <form>
-            <div class="text-center space-x-4 md:space-x-8 mt-14 pb-28">
-              <input class="inline-block w-3/5 py-2 rounded-xl font-bold bg-yellow-300 md:text-4xl text-3xl text-blue-800 cursor-pointer hover:bg-yellow-200 hover:text-blue-600 active:bg-red-200" type="button" value="トップへ" />
-            </div>
-          </form>
-        </div>
+        <form>
+          <div class="text-center space-x-4 md:space-x-8 mt-14 pb-28">
+            <input class="inline-block w-40 py-2 rounded-xl font-bold bg-yellow-300 md:text-4xl text-3xl text-blue-800 cursor-pointer hover:bg-yellow-200 hover:text-blue-600 active:bg-red-200" type="button" value="戻　る" />
+          </div>
+        </form>
       </div>
     </div>
   </main>
