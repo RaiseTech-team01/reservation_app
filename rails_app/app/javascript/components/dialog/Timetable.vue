e5c196a31d0fe70b9c81ac4e9e4c87cafa744bae
Nishimura <yukinkopen@gmail.com>
2021-06-08 11:43:10 +0900

    [mod] タイムテーブルの実装

--- rails_app/app/javascript/components/dialog/Timetable.vue
+++ rails_app/app/javascript/components/dialog/Timetable.vue
@@ -1,117 +1,27 @@
 <template>
 <div>
-  <div id="calendar-bg" class="hidden absolute w-full h-full z-10  bg-black opacity-50"></div>
-  <div id="calendar-dialog" class="hidden absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 border-2 border-black text-center p-8 bg-white">
-    <div class="absolute right-4 top-2 text-blue-800 text-4xl cursor-pointer" @click="dismiss_calendar">
+  <div id="timetable-bg" class="hidden absolute w-full h-full z-10  bg-black opacity-50"></div>
+  <div id="timetable-dialog" class="hidden absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10 border-2 border-black text-center p-8 bg-white">
+    <div class="absolute right-4 top-2 text-blue-800 text-4xl cursor-pointer" @click="dismiss_timetable">
       <i class="fas fa-times "></i>
     </div>
-    <p class="text-blue-800 text-xl font-bold text-center w-auto py-1">2021年</p>
+    <p class="text-blue-800 text-xl font-bold text-center w-auto py-1">2021年3月</p>
     <p class="text-center text-blue-800 font-bold text-4xl py-2">
       <span class="inline-block text-5xl pr-2 align-middle"><i class="fas fa-caret-left"></i></span>
-      <span class="align-middle">3月</span>
+      <span class="align-middle">5日</span>
       <span class="inline-block text-5xl pl-2 align-middle"><i class="fas fa-caret-right"></i></span>
     </p>
-    <table class="text-center inline-block mt-4">
-      <tr>
-        <td class="text-red-600 text-xl font-bold p-3">Su</td>
-        <td class="text-blue-800 text-xl font-bold p-3">Mo</td>
-        <td class="text-blue-800 text-xl font-bold p-3">Tu</td>
-        <td class="text-blue-800 text-xl font-bold p-3">We</td>
-        <td class="text-blue-800 text-xl font-bold p-3">Th</td>
-        <td class="text-blue-800 text-xl font-bold p-3">Fr</td>
-        <td class="text-blue-800 text-xl font-bold p-3">Sa</td>
-      </tr>
-      <tr>
-        <td class="text-blue-800 text-xl px-3 pt-3">28</td>
-        <td class="text-blue-800 text-xl px-3 pt-3">1</td>
-        <td class="text-blue-800 text-xl px-3 pt-3">2</td>
-        <td class="text-blue-800 text-xl px-3 pt-3">3</td>
-        <td class="text-blue-800 text-xl px-3 pt-3">4</td>
-        <td class="text-blue-800 text-xl px-3 pt-3">5</td>
-        <td class="text-blue-800 text-xl px-3 pt-3">6</td>
-      </tr>
-      <tr>
-        <td class="text-red-600 text-xl px-3 pb-2">o</td>
-        <td class="text-red-600 text-xl px-3 pb-2">x</td>
-        <td class="text-red-600 text-xl px-3 pb-2">o</td>
-        <td class="text-red-600 text-xl px-3 pb-2">o</td>
-        <td class="text-red-600 text-xl px-3 pb-2">o</td>
-        <td class="text-red-600 text-xl px-3 pb-2">o</td>
-        <td class="text-red-600 text-xl px-3 pb-2">-</td>
-      </tr>
-      <tr>
-        <td class="text-blue-800 text-xl px-3 pt-3">7</td>
-        <td class="text-blue-800 text-xl px-3 pt-3">8</td>
-        <td class="text-blue-800 text-xl px-3 pt-3">9</td>
-        <td class="text-blue-800 text-xl px-3 pt-3">10</td>
-        <td class="text-blue-800 text-xl px-3 pt-3">11</td>
-        <td class="text-blue-800 text-xl px-3 pt-3">12</td>
-        <td class="text-blue-800 text-xl px-3 pt-3">13</td>
-      </tr>
-      <tr>
-        <td class="text-red-600 text-xl px-3 pb-2">o</td>
-        <td class="text-red-600 text-xl px-3 pb-2">x</td>
-        <td class="text-red-600 text-xl px-3 pb-2">o</td>
-        <td class="text-red-600 text-xl px-3 pb-2">o</td>
-        <td class="text-red-600 text-xl px-3 pb-2">o</td>
-        <td class="text-red-600 text-xl px-3 pb-2">o</td>
-        <td class="text-red-600 text-xl px-3 pb-2">-</td>
-      </tr>
-      <tr>
-        <td class="text-blue-800 text-xl px-3 pt-3">14</td>
-        <td class="text-blue-800 text-xl px-3 pt-3">15</td>
-        <td class="text-blue-800 text-xl px-3 pt-3">16</td>
-        <td class="text-blue-800 text-xl px-3 pt-3">17</td>
-        <td class="text-blue-800 text-xl px-3 pt-3">18</td>
-        <td class="text-blue-800 text-xl px-3 pt-3">19</td>
-        <td class="text-blue-800 text-xl px-3 pt-3">20</td>
-      </tr>
-      <tr>
-        <td class="text-red-600 text-xl px-3 pb-2">o</td>
-        <td class="text-red-600 text-xl px-3 pb-2">x</td>
-        <td class="text-red-600 text-xl px-3 pb-2">o</td>
-        <td class="text-red-600 text-xl px-3 pb-2">o</td>
-        <td class="text-red-600 text-xl px-3 pb-2">o</td>
-        <td class="text-red-600 text-xl px-3 pb-2">o</td>
-        <td class="text-red-600 text-xl px-3 pb-2">-</td>
-      </tr>
-      <tr>
-        <td class="text-blue-800 text-xl px-3 pt-3">21</td>
-        <td class="text-blue-800 text-xl px-3 pt-3">22</td>
-        <td class="text-blue-800 text-xl px-3 pt-3">23</td>
-        <td class="text-blue-800 text-xl px-3 pt-3">24</td>
-        <td class="text-blue-800 text-xl px-3 pt-3">25</td>
-        <td class="text-blue-800 text-xl px-3 pt-3">26</td>
-        <td class="text-blue-800 text-xl px-3 pt-3">27</td>
-      </tr>
-      <tr>
-        <td class="text-red-600 text-xl px-3 pb-2">o</td>
-        <td class="text-red-600 text-xl px-3 pb-2">x</td>
-        <td class="text-red-600 text-xl px-3 pb-2">o</td>
-        <td class="text-red-600 text-xl px-3 pb-2">o</td>
-        <td class="text-red-600 text-xl px-3 pb-2">o</td>
-        <td class="text-red-600 text-xl px-3 pb-2">o</td>
-        <td class="text-red-600 text-xl px-3 pb-2">-</td>
-      </tr>
-      <tr>
-        <td class="text-blue-800 text-xl px-3 pt-3">28</td>
-        <td class="text-blue-800 text-xl px-3 pt-3">29</td>
-        <td class="text-blue-800 text-xl px-3 pt-3">30</td>
-        <td class="text-blue-800 text-xl px-3 pt-3">31</td>
-        <td class="text-blue-800 text-xl px-3 pt-3">1</td>
-        <td class="text-blue-800 text-xl px-3 pt-3">2</td>
-        <td class="text-blue-800 text-xl px-3 pt-3">3</td>
-      </tr>
-      <tr>
-        <td class="text-red-600 text-xl px-3 pb-2">o</td>
-        <td class="text-red-600 text-xl px-3 pb-2">x</td>
-        <td class="text-red-600 text-xl px-3 pb-2">o</td>
-        <td class="text-red-600 text-xl px-3 pb-2">o</td>
-        <td class="text-red-600 text-xl px-3 pb-2">o</td>
-        <td class="text-red-600 text-xl px-3 pb-2">o</td>
-        <td class="text-red-600 text-xl px-3 pb-2">-</td>
-      </tr>
-    </table>
+    <div class="inline-block mt-4 bg-gray-100 px-2 pt-2 pb-5">
+      <p class="relative w-96 text-2xl text-blue-800 font-bold py-2"><span class="absolute inline-block text-lg font-bold -top-1 z-10 bg-gray-100 p-2 left-0">16:45</span></p>
+      <p class="relative w-96 text-2xl text-blue-800 font-bold border-t border-blue-800 py-2"><span class="absolute inline-block text-lg font-bold top-6 z-10 bg-gray-100 p-2 left-0">17:00</span>不可</p>
+      <p class="relative w-96 text-2xl text-blue-800 font-bold border-t border-blue-800 py-2"><span class="absolute inline-block text-lg font-bold top-6 z-10 bg-gray-100 p-2 left-0">17:15</span>不可</p>
+      <p class="relative w-96 text-2xl text-blue-800 font-bold border-t border-blue-800 py-2"><span class="absolute inline-block text-lg font-bold top-6 z-10 bg-gray-100 p-2 left-0">17:30</span>可</p>
+      <p class="relative w-96 text-2xl text-blue-800 font-bold border-t border-blue-800 py-2"><span class="absolute inline-block text-lg font-bold top-6 z-10 bg-gray-100 p-2 left-0">17:45</span>可</p>
+      <p class="relative w-96 text-2xl text-blue-800 font-bold border-t border-blue-800 py-2"><span class="absolute inline-block text-lg font-bold top-6 z-10 bg-gray-100 p-2 left-0">18:00</span>可</p>
+      <p class="relative w-96 text-2xl text-blue-800 font-bold border-t border-blue-800 py-2"><span class="absolute inline-block text-lg font-bold top-6 z-10 bg-gray-100 p-2 left-0">18:15</span>可</p>
+      <p class="relative w-96 text-2xl text-blue-800 font-bold border-t border-blue-800 py-2"><span class="absolute inline-block text-lg font-bold top-6 z-10 bg-gray-100 p-2 left-0">18:30</span>可</p>
+      <p class="relative w-96 text-2xl text-blue-800 font-bold border-t border-b border-blue-800 py-2"><span class="absolute inline-block text-lg font-bold top-6 z-10 bg-gray-100 p-2 left-0">18:45</span>可</p>
+    </div>
   </div>
 </div>
 </template>
@@ -119,10 +29,10 @@
 <script>
 export default {
   methods: {
-    dismiss_calendar() {
-      if ($("#calendar-dialog").is(':visible')) {
-        $("#calendar-bg").hide()
-        $("#calendar-dialog").hide("normal", function() {
+    dismiss_timetable() {
+      if ($("#timetable-dialog").is(':visible')) {
+        $("#timetable-bg").hide()
+        $("#timetable-dialog").hide("normal", function() {
           $('body, html').css({"overflow": "visible", "height": "auto"});
         });
       }
