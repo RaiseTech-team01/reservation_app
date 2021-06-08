2f699fac292d94591c4fde1e8686176082a9fac9
Nishimura <yukinkopen@gmail.com>
2021-06-08 12:55:56 +0900

    [RAISETECH-74] 予約一覧画面 (#101)
    * [add] 予約一覧画面を追加
      - 予約完了画面のコードから複写
    
    * [mod] 予約一覧画面のレイアウトに合わせて修正
      - テーブルを定義
      - 見本となる文字列を設定
      - レスポンシブ対応
    
    * [mod] レイアウトの調整

--- rails_app/config/routes.rb
+++ rails_app/config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
   get "/reservation_form", to: "home#top"
   get "/reservation_confirm", to: "home#top"
   get "/reservation_complete", to: "home#top"
+  get "/reservation_list", to: "home#top"
 
   namespace :api do
     namespace :v1 do
