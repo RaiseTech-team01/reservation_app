3586c74b516b341a7651fb1d68cb85286b6c23c8
KazusaNakagawa <kazusanakagawa@gmail.com>
2021-06-11 20:51:20 +0900

    [fix] 予約店舗の座席を操作できるように修正

--- rails_app/app/controllers/api/v1/reservations_controller.rb
+++ rails_app/app/controllers/api/v1/reservations_controller.rb
@@ -51,8 +51,10 @@ class Api::V1::ReservationsController < Api::V1::BaseApiController
     reservations = current_user.reservations.where(store_id: params[:store_id])
     reservation = reservations.find(params[:id])
 
-    # 予約席を増やす
-    Store.find(params[:store_id]).seat += reservation[:number_people]
+    # 予約席をキャンセル分増やす
+    search_store = Store.find(params[:store_id])
+    residual_seat = search_store.seat + reservation[:number_people]
+    search_store.update!(seat: residual_seat)
 
     reservation.destroy!
   end
