84445ecd9d124d73d1103843ef4644087ab0d33b
Nishimura <yukinkopen@gmail.com>
2021-06-15 02:31:24 +0900

    [mod] 画面の中身を修正   - 文字列などを修正   - 不要なコードを削除

--- rails_app/app/javascript/components/store/StoreReservationList.vue
+++ rails_app/app/javascript/components/store/StoreReservationList.vue
@@ -6,130 +6,60 @@
   <div class="container-fluid">
     <div class="row">
       <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-        <h2>Section title</h2>
+        <h2>予約一覧画面</h2>
         <div class="table-responsive">
           <table class="table table-striped table-sm">
             <thead>
               <tr>
                 <th>#</th>
-                <th>Header</th>
-                <th>Header</th>
-                <th>Header</th>
-                <th>Header</th>
+                <th>日付</th>
+                <th>予約者名</th>
+                <th>時間帯</th>
+                <th>予約人数</th>
               </tr>
             </thead>
             <tbody>
               <tr>
                 <td>1,001</td>
-                <td>random</td>
-                <td>data</td>
-                <td>placeholder</td>
-                <td>text</td>
+                <td>2021/06/15</td>
+                <td>田中 一郎</td>
+                <td>18:00~</td>
+                <td>2</td>
               </tr>
               <tr>
                 <td>1,002</td>
-                <td>placeholder</td>
-                <td>irrelevant</td>
-                <td>visual</td>
-                <td>layout</td>
+                <td>2021/06/15</td>
+                <td>鈴木 次郎</td>
+                <td>19:00~</td>
+                <td>5</td>
               </tr>
               <tr>
                 <td>1,003</td>
-                <td>data</td>
-                <td>rich</td>
-                <td>dashboard</td>
-                <td>tabular</td>
+                <td>2021/06/16</td>
+                <td>森田 三郎</td>
+                <td>18:00~</td>
+                <td>30</td>
               </tr>
               <tr>
                 <td>1,003</td>
-                <td>information</td>
-                <td>placeholder</td>
-                <td>illustrative</td>
-                <td>data</td>
+                <td>2021/06/16</td>
+                <td>久保田 史郎</td>
+                <td>23:30~</td>
+                <td>3</td>
               </tr>
               <tr>
                 <td>1,004</td>
-                <td>text</td>
-                <td>random</td>
-                <td>layout</td>
-                <td>dashboard</td>
+                <td>2021/06/18</td>
+                <td>吉本 六郎</td>
+                <td>19:30~</td>
+                <td>4</td>
               </tr>
               <tr>
                 <td>1,005</td>
-                <td>dashboard</td>
-                <td>irrelevant</td>
-                <td>text</td>
-                <td>placeholder</td>
-              </tr>
-              <tr>
-                <td>1,006</td>
-                <td>dashboard</td>
-                <td>illustrative</td>
-                <td>rich</td>
-                <td>data</td>
-              </tr>
-              <tr>
-                <td>1,007</td>
-                <td>placeholder</td>
-                <td>tabular</td>
-                <td>information</td>
-                <td>irrelevant</td>
-              </tr>
-              <tr>
-                <td>1,008</td>
-                <td>random</td>
-                <td>data</td>
-                <td>placeholder</td>
-                <td>text</td>
-              </tr>
-              <tr>
-                <td>1,009</td>
-                <td>placeholder</td>
-                <td>irrelevant</td>
-                <td>visual</td>
-                <td>layout</td>
-              </tr>
-              <tr>
-                <td>1,010</td>
-                <td>data</td>
-                <td>rich</td>
-                <td>dashboard</td>
-                <td>tabular</td>
-              </tr>
-              <tr>
-                <td>1,011</td>
-                <td>information</td>
-                <td>placeholder</td>
-                <td>illustrative</td>
-                <td>data</td>
-              </tr>
-              <tr>
-                <td>1,012</td>
-                <td>text</td>
-                <td>placeholder</td>
-                <td>layout</td>
-                <td>dashboard</td>
-              </tr>
-              <tr>
-                <td>1,013</td>
-                <td>dashboard</td>
-                <td>irrelevant</td>
-                <td>text</td>
-                <td>visual</td>
-              </tr>
-              <tr>
-                <td>1,014</td>
-                <td>dashboard</td>
-                <td>illustrative</td>
-                <td>rich</td>
-                <td>data</td>
-              </tr>
-              <tr>
-                <td>1,015</td>
-                <td>random</td>
-                <td>tabular</td>
-                <td>information</td>
-                <td>text</td>
+                <td>2021/06/19</td>
+                <td>川上 七郎</td>
+                <td>18:45~</td>
+                <td>8</td>
               </tr>
             </tbody>
           </table>
