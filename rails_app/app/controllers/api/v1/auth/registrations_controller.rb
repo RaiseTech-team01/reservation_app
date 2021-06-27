0938d1f9d077b7775c999e86f1006e20fda7783e
TOBE-ASUNARO <ghnoadd@m5.asunaro-work.com>
2021-06-27 18:12:55 +0900

    [add] check_token-before_userRegistCntr

--- rails_app/app/controllers/api/v1/auth/registrations_controller.rb
+++ rails_app/app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,7 +1,14 @@
 class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
   skip_before_action :verify_authenticity_token
+  before_action :check_token
 
   private
+
+    def check_token
+      if request.headers["access-token"].present? || request.headers["uid"].present? || request.headers["client"].present? 
+        render json: { errors: { title: '既にログインしています', detail: 'ユーザー登録は行われています。' } }, status: 400
+      end
+    end
 
     def sign_up_params
       params.permit(:name, :email, :furigana, :tel, :birthday, :gender, :address, :password, :password_confirmation)
