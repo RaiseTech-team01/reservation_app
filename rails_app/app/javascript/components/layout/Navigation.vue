9a0f235127875f8e8d891cac18903db5c3d09abc
Nishimura <yukinkopen@gmail.com>
2021-07-03 05:53:55 +0900

    [mod] ログイン画面のレイアウトを修正

--- rails_app/app/javascript/components/layout/Navigation.vue
+++ rails_app/app/javascript/components/layout/Navigation.vue
@@ -1,10 +1,30 @@
 <template>
   <div class="h-16 text-center flex flex-row justify-center">
-    <span class="flex flex-col justify-center w-1/6 mr-1 h-16 text-2xl bg-red-500 hover:bg-red-700 active:bg-yellow-700 text-white cursor-pointer" @click="goToReservationForm">予約入力</span>
-    <span class="flex flex-col justify-center w-1/6 mr-1 h-16 text-2xl bg-red-500 hover:bg-red-700 active:bg-yellow-700 text-white cursor-pointer" @click="goToReservationList">予約一覧</span>
-    <span class="flex flex-col justify-center w-1/6 mr-1 h-16 text-2xl bg-red-500 hover:bg-red-700 active:bg-yellow-700 text-white cursor-pointer" @click="goToReservationViewHistory">閲覧履歴</span>
-    <span class="flex flex-col justify-center w-1/6 mr-1 h-16 text-2xl bg-red-500 hover:bg-red-700 active:bg-yellow-700 text-white cursor-pointer" @click="goToReservationHistory">予約履歴</span>
-    <span class="flex flex-col justify-center w-1/6 mr-1 h-16 text-2xl bg-red-500 hover:bg-red-700 active:bg-yellow-700 text-white font-bold cursor-pointer" @click="goToAccountSettings">アカウント設定</span>
+    <span
+      class="flex flex-col justify-center w-1/6 mr-1 h-16 text-2xl bg-red-500 hover:bg-red-700 active:bg-yellow-700 text-white cursor-pointer"
+      @click="goToReservationForm"
+      >予約入力</span
+    >
+    <span
+      class="flex flex-col justify-center w-1/6 mr-1 h-16 text-2xl bg-red-500 hover:bg-red-700 active:bg-yellow-700 text-white cursor-pointer"
+      @click="goToReservationList"
+      >予約一覧</span
+    >
+    <span
+      class="flex flex-col justify-center w-1/6 mr-1 h-16 text-2xl bg-red-500 hover:bg-red-700 active:bg-yellow-700 text-white cursor-pointer"
+      @click="goToReservationViewHistory"
+      >閲覧履歴</span
+    >
+    <span
+      class="flex flex-col justify-center w-1/6 mr-1 h-16 text-2xl bg-red-500 hover:bg-red-700 active:bg-yellow-700 text-white cursor-pointer"
+      @click="goToReservationHistory"
+      >予約履歴</span
+    >
+    <span
+      class="flex flex-col justify-center w-1/6 mr-1 h-16 text-2xl bg-red-500 hover:bg-red-700 active:bg-yellow-700 text-white font-bold cursor-pointer"
+      @click="goToAccountSettings"
+      >アカウント設定</span
+    >
   </div>
 </template>
 
