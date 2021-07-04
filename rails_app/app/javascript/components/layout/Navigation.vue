3b57155b1610456e9ea5cfa9d11aa50fbf500dad
TOBE-ASUNARO <ghnoadd@m5.asunaro-work.com>
2021-07-04 21:43:06 +0900

    naviMenu

--- rails_app/app/javascript/components/layout/Navigation.vue
+++ rails_app/app/javascript/components/layout/Navigation.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-16 text-center flex flex-row justify-center">
+  <div  v-if="isLogin === true"  class="h-16 text-center flex flex-row justify-center">
     <span class="flex flex-col justify-center w-1/6 mr-1 h-16 text-2xl bg-red-500 hover:bg-red-700 active:bg-yellow-700 text-white cursor-pointer" @click="goToReservationForm">予約入力</span>
     <span class="flex flex-col justify-center w-1/6 mr-1 h-16 text-2xl bg-red-500 hover:bg-red-700 active:bg-yellow-700 text-white cursor-pointer" @click="goToReservationList">予約一覧</span>
     <span class="flex flex-col justify-center w-1/6 mr-1 h-16 text-2xl bg-red-500 hover:bg-red-700 active:bg-yellow-700 text-white cursor-pointer" @click="goToReservationViewHistory">閲覧履歴</span>
@@ -15,6 +15,12 @@ export default {
   data: function () {
     return {
     }
+  },
+  computed: {
+    isLogin() {
+      // TODO ログイン状態を真偽値で返す
+      return this.$store.getters.auth.isLogin;
+    },
   },
   methods: {
     goToReservationForm() {
