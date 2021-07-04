7c75d8d2ce61313bfa9bb4438a675ce873b8fd55
TOBE <71219657+TOBE-ASUNARO@users.noreply.github.com>
2021-07-05 03:35:42 +0900

    R84 login page fixed menubar (#124)
    * [fix]menu-bar
    * [fix]Remove duplicate lines in Navigation.vue

--- rails_app/app/javascript/components/layout/Navigation.vue
+++ rails_app/app/javascript/components/layout/Navigation.vue
@@ -1,30 +1,10 @@
 <template>
-  <div class="h-16 text-center flex flex-row justify-center">
-    <span
-      class="flex flex-col justify-center w-1/6 mr-1 h-16 text-2xl bg-red-500 hover:bg-red-700 active:bg-yellow-700 text-white cursor-pointer"
-      @click="goToReservationForm"
-      >予約入力</span
-    >
-    <span
-      class="flex flex-col justify-center w-1/6 mr-1 h-16 text-2xl bg-red-500 hover:bg-red-700 active:bg-yellow-700 text-white cursor-pointer"
-      @click="goToReservationList"
-      >予約一覧</span
-    >
-    <span
-      class="flex flex-col justify-center w-1/6 mr-1 h-16 text-2xl bg-red-500 hover:bg-red-700 active:bg-yellow-700 text-white cursor-pointer"
-      @click="goToReservationViewHistory"
-      >閲覧履歴</span
-    >
-    <span
-      class="flex flex-col justify-center w-1/6 mr-1 h-16 text-2xl bg-red-500 hover:bg-red-700 active:bg-yellow-700 text-white cursor-pointer"
-      @click="goToReservationHistory"
-      >予約履歴</span
-    >
-    <span
-      class="flex flex-col justify-center w-1/6 mr-1 h-16 text-2xl bg-red-500 hover:bg-red-700 active:bg-yellow-700 text-white font-bold cursor-pointer"
-      @click="goToAccountSettings"
-      >アカウント設定</span
-    >
+  <div  v-if="isLogin === true"  class="h-16 text-center flex flex-row justify-center">
+    <span class="flex flex-col justify-center w-1/6 mr-1 h-16 text-2xl bg-red-500 hover:bg-red-700 active:bg-yellow-700 text-white cursor-pointer" @click="goToReservationForm">予約入力</span>
+    <span class="flex flex-col justify-center w-1/6 mr-1 h-16 text-2xl bg-red-500 hover:bg-red-700 active:bg-yellow-700 text-white cursor-pointer" @click="goToReservationList">予約一覧</span>
+    <span class="flex flex-col justify-center w-1/6 mr-1 h-16 text-2xl bg-red-500 hover:bg-red-700 active:bg-yellow-700 text-white cursor-pointer" @click="goToReservationViewHistory">閲覧履歴</span>
+    <span class="flex flex-col justify-center w-1/6 mr-1 h-16 text-2xl bg-red-500 hover:bg-red-700 active:bg-yellow-700 text-white cursor-pointer" @click="goToReservationHistory">予約履歴</span>
+    <span class="flex flex-col justify-center w-1/6 mr-1 h-16 text-2xl bg-red-500 hover:bg-red-700 active:bg-yellow-700 text-white font-bold cursor-pointer" @click="goToAccountSettings">アカウント設定</span>
   </div>
 </template>
 
@@ -35,6 +15,12 @@ export default {
   data: function () {
     return {
     }
+  },
+  computed: {
+    isLogin() {
+      // TODO ログイン状態を真偽値で返す
+      return this.$store.getters.auth.isLogin;
+    },
   },
   methods: {
     goToReservationForm() {
@@ -55,3 +41,4 @@ export default {
   },
 }
 </script>
+
