221ed29e9317ef5af85f182bcdff3214e4d46ae7
TOBE <71219657+TOBE-ASUNARO@users.noreply.github.com>
2021-07-05 10:38:18 +0900

    Cherry-pick form main repo. it is that [bug] 不正なユーザー登録がされてしまう ＃116 - Draft check Done (#125)
    * [add] check_token-before_userRegistCntr
    * [fix] err message update for check_token-before_userRegistCntr

--- rails_app/app/controllers/api/v1/auth/registrations_controller.rb
+++ rails_app/app/controllers/api/v1/auth/registrations_controller.rb
@@ -1,7 +1,14 @@
 class Api::V1::Auth::RegistrationsController < DeviseTokenAuth::RegistrationsController
   skip_before_action :verify_authenticity_token
+  before_action :check_token
 
   private
+
+    def check_token
+      if request.headers["access-token"].present? || request.headers["uid"].present? || request.headers["client"].present?
+        render json: { errors: { title: "不正なトークンがあります。", detail: "操作の確認をおねがいします。" } }, status: 400
+      end
+    end
 
     def sign_up_params
       params.permit(:name, :email, :furigana, :tel, :birthday, :gender, :address, :password, :password_confirmation)
