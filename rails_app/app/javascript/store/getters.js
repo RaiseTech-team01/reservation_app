d61cf58ce8d2cd7c4881240775202996e911da46
Nishimura <yukinkopen@gmail.com>
2021-07-18 00:41:59 +0900

    [mod] フォーマット適用

--- rails_app/app/javascript/store/getters.js
+++ rails_app/app/javascript/store/getters.js
@@ -2,7 +2,6 @@ const getters = {
     userData: state => state.userData,
     auth: state => state.auth,
     registrationUserData: state => state.registrationUserData,
-  // ここに追記していく
-}
-export default getters
-
+    // ここに追記していく
+};
+export default getters;
