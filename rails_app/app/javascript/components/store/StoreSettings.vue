a69b8c8fe826ff8e8abef552728eb53fd6251e36
Yuki Nishimura <yukinkopen@gmail.com>
2021-07-29 07:57:30 +0900

    [mod] エラー表示を追加＆非同期処理用のコードをコピペ

--- rails_app/app/javascript/components/store/StoreSettings.vue
+++ rails_app/app/javascript/components/store/StoreSettings.vue
@@ -5,10 +5,13 @@
     </dir>
     <div class="container">
       <main>
-        <div class="py-5 text-center">
+        <div class="mt-4 py-5 text-center">
           <h2>設定</h2>
         </div>
         <div class="row g-5 flex justify-center">
+          <div class="col-12 alert alert-danger" v-show="hasError()">
+            <p>{{ errorMessage }}</p>
+          </div>
           <div class="col-md-7 col-lg-8">
             <form class="needs-validation" @submit="validate" novalidate>
               <div class="row g-3">
@@ -44,10 +47,13 @@
 
 <script>
 import StoreHeader from '../layout/StoreHeader.vue'
+import axios from 'axios'
 
 export default {
   data: function () {
-    return {}
+    return {
+      errorMessage: '111',
+    }
   },
 
   components: {
@@ -55,6 +61,9 @@ export default {
   },
 
   methods: {
+    hasError() {
+      return !!this.errorMessage
+    },
     validate(event) {
       if (!event.target.checkValidity()) {
         event.preventDefault()
@@ -62,9 +71,40 @@ export default {
       }
       event.target.classList.add('was-validated')
     },
+    async requestDeleteAccount() {
+      // TODO 削除APIを利用する
+      await axios
+        .get(
+          '/api/v1/stores/reservations',
+          { headers: this.requestHeaders },
+          { data: {} }
+        )
+        .then((response) => {
+          this.errorMessage = ''
+          const reserveData = response.data.map((rsrv) => {
+            return {
+              id: rsrv.id,
+              date: this.getDateString(rsrv),
+              user: rsrv.user.name,
+              startTime: this.getTimeString(rsrv),
+              seatNum: rsrv.number_people,
+            }
+          })
+          reserveData.forEach((item) => {
+            this.addData(item)
+          })
+        })
+        .catch((error) => {
+          this.errorMessage = error.response.data.errors[0]
+        })
+        .finally(() => {
+          this.loading = false
+        })
+    },
     deleteAccount() {
-      // TODO アカウントを削除する
-      alert('TODO: アカウントを削除する')
+      if (!confirm('本当にアカウントを削除してよろしいですか？')) {
+        return
+      }
     },
   },
 }
@@ -74,7 +114,7 @@ export default {
 <style scoped src="../../../assets/stylesheets/offcanvas.css"></style>
 <style scoped>
 p {
-  font-size: 1.5em;
+  font-size: 1em;
   text-align: center;
 }
 
