6b9571fe88c1613ec94938321f6bacd7607e4091
Yuki Nishimura <yukinkopen@gmail.com>
2021-07-29 06:54:38 +0900

    [mod] 予約一覧APIを利用して取得したデータを表示

--- rails_app/app/javascript/components/store/StoreReservationList.vue
+++ rails_app/app/javascript/components/store/StoreReservationList.vue
@@ -8,6 +8,9 @@
         <main class="col-md-9">
           <h2>予約一覧画面</h2>
           <div class="table-responsive">
+            <div class="col-12 alert alert-danger" v-show="hasError()">
+              <p>{{ errorMessage }}</p>
+            </div>
             <table class="table table-striped table-sm">
               <thead>
                 <tr>
@@ -37,11 +40,15 @@
 
 <script>
 import StoreHeader from '../layout/StoreHeader.vue'
+import axios from 'axios'
 
 export default {
   data: function () {
     return {
+      loading: false,
+      errorMessage: '',
       reservationList: [],
+      requestHeaders: {},
     }
   },
 
@@ -50,6 +57,9 @@ export default {
   },
 
   methods: {
+    hasError() {
+      return !!this.errorMessage
+    },
     validate(event) {
       if (!event.target.checkValidity()) {
         event.preventDefault()
@@ -80,11 +90,91 @@ export default {
         seatNum: 25,
       })
     },
+    getDateString(reserveData) {
+      // date_at: "2021-01-01T00:00:00.000+09:00"
+      const date = reserveData.date_at
+      const str = date.match(/^(2[0-9]+)-([01][0-9])-([0-3][0-9])/)
+      return str[1] + '/' + str[2] + '/' + str[3]
+    },
+    getTimeString(reserveData) {
+      // date_at: "2021-01-01T00:00:00.000+09:00"
+      const date = reserveData.date_at
+      const str = date.match(/T([0-2][0-9]):([0-5][0-9])/)
+      return str[1] + ':' + str[2]
+    },
+    async requestRsrvData() {
+      console.log('requestRsrvData')
+      this.loading = true
+
+      this.requestHeaders['access-token'] = 'fUt09hKBSccZ_Bw0dnPizQ'
+      // this.requestHeaders['access-token'] = 'fUt09hKBSccZ_Bw0dnPizQasfsdfsd'
+      this.requestHeaders['client'] = 'KyFzCkpSzX3mwsOkmVB3YA'
+      // this.requestHeaders['uid'] = 'store1@sample.com'
+      this.requestHeaders['uid'] = 'store1@sample.com'
+
+      console.log(this.requestHeaders)
+
+      await axios
+        .get(
+          '/api/v1/stores/reservations',
+          { headers: this.requestHeaders },
+          { data: {} }
+        )
+        .then((response) => {
+          console.log('res', response)
+          this.errorMessage = ''
+
+          const reserveData = response.data.map((rsrv) => {
+            return {
+              id: rsrv.id,
+              date: this.getDateString(rsrv),
+              user: rsrv.user.name,
+              startTime: this.getTimeString(rsrv),
+              seatNum: rsrv.number_people,
+            }
+          })
+          reserveData.forEach((item) => {
+            this.reservationList.push(item)
+          })
+
+          // Vuex store
+          // this.$store.dispatch('storeAuth/updateLogin', true)
+          // this.$store.dispatch('registrationStoreUserData/updateErr', '')
+          // console.log(response)
+          // localStorage.setItem(
+          //   'store-access-token',
+          //   response.headers['access-token']
+          // )
+          // localStorage.setItem('store-uid', response.headers['uid'])
+          // localStorage.setItem('store-client', response.headers['client'])
+          // Router.push('/store_dash_board')
+        })
+        .catch((error) => {
+          console.log('error')
+          // console.log('err', error.response)
+          // console.log('err', error.response.data.errors[0])
+          this.errorMessage = error.response.data.errors[0]
+          // console.log(error.response.status)
+          // console.log(error.response.data.errors.full_messages)
+          // this.$store.dispatch(
+          //   'registrationStoreUserData/updateErr',
+          //   error.response.data.errors.full_messages
+          // )
+          // Router.push('/store_account_form')
+        })
+        .finally(() => {
+          this.loading = false
+        })
+    },
     initialize() {
-      this.addData()
+      console.log('init')
+      // this.addData()
+      this.errorMessage = 'sample'
+      this.requestRsrvData()
     },
   },
   mounted() {
+    console.log('mounted')
     this.initialize()
   },
 }
@@ -94,7 +184,7 @@ export default {
 <style scoped src="../../../assets/stylesheets/dashboard.css"></style>
 <style scoped>
 p {
-  font-size: 2em;
+  font-size: 1em;
   text-align: center;
 }
 
