dfcb7074bc63df0f81107f7a9cfff7424a287fcf
Yuki Nishimura <yukinkopen@gmail.com>
2021-07-28 17:52:35 +0900

    [mod] フォーマット適用

--- rails_app/app/javascript/components/store/StoreReservationList.vue
+++ rails_app/app/javascript/components/store/StoreReservationList.vue
@@ -1,82 +1,81 @@
 <template>
-<div class="main m-0">
-  <dir class="storeheader m-0 text-center">
-    <StoreHeader />
-  </dir>
-  <div class="container-fluid mt-20">
-    <div class="row flex justify-center">
-      <main class="col-md-9">
-        <h2>予約一覧画面</h2>
-        <div class="table-responsive">
-          <table class="table table-striped table-sm">
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>日付</th>
-                <th>予約者名</th>
-                <th>時間帯</th>
-                <th>予約人数</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1,001</td>
-                <td>2021/06/15</td>
-                <td>田中 一郎</td>
-                <td>18:00~</td>
-                <td>2</td>
-              </tr>
-              <tr>
-                <td>1,002</td>
-                <td>2021/06/15</td>
-                <td>鈴木 次郎</td>
-                <td>19:00~</td>
-                <td>5</td>
-              </tr>
-              <tr>
-                <td>1,003</td>
-                <td>2021/06/16</td>
-                <td>森田 三郎</td>
-                <td>18:00~</td>
-                <td>30</td>
-              </tr>
-              <tr>
-                <td>1,003</td>
-                <td>2021/06/16</td>
-                <td>久保田 史郎</td>
-                <td>23:30~</td>
-                <td>3</td>
-              </tr>
-              <tr>
-                <td>1,004</td>
-                <td>2021/06/18</td>
-                <td>吉本 六郎</td>
-                <td>19:30~</td>
-                <td>4</td>
-              </tr>
-              <tr>
-                <td>1,005</td>
-                <td>2021/06/19</td>
-                <td>川上 七郎</td>
-                <td>18:45~</td>
-                <td>8</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </main>
+  <div class="main m-0">
+    <dir class="storeheader m-0 text-center">
+      <StoreHeader />
+    </dir>
+    <div class="container-fluid mt-20">
+      <div class="row flex justify-center">
+        <main class="col-md-9">
+          <h2>予約一覧画面</h2>
+          <div class="table-responsive">
+            <table class="table table-striped table-sm">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>日付</th>
+                  <th>予約者名</th>
+                  <th>時間帯</th>
+                  <th>予約人数</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1,001</td>
+                  <td>2021/06/15</td>
+                  <td>田中 一郎</td>
+                  <td>18:00~</td>
+                  <td>2</td>
+                </tr>
+                <tr>
+                  <td>1,002</td>
+                  <td>2021/06/15</td>
+                  <td>鈴木 次郎</td>
+                  <td>19:00~</td>
+                  <td>5</td>
+                </tr>
+                <tr>
+                  <td>1,003</td>
+                  <td>2021/06/16</td>
+                  <td>森田 三郎</td>
+                  <td>18:00~</td>
+                  <td>30</td>
+                </tr>
+                <tr>
+                  <td>1,003</td>
+                  <td>2021/06/16</td>
+                  <td>久保田 史郎</td>
+                  <td>23:30~</td>
+                  <td>3</td>
+                </tr>
+                <tr>
+                  <td>1,004</td>
+                  <td>2021/06/18</td>
+                  <td>吉本 六郎</td>
+                  <td>19:30~</td>
+                  <td>4</td>
+                </tr>
+                <tr>
+                  <td>1,005</td>
+                  <td>2021/06/19</td>
+                  <td>川上 七郎</td>
+                  <td>18:45~</td>
+                  <td>8</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </main>
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
-import StoreHeader from "../layout/StoreHeader.vue"
+import StoreHeader from '../layout/StoreHeader.vue'
 
 export default {
   data: function () {
-    return {
-    }
+    return {}
   },
 
   components: {
@@ -90,8 +89,8 @@ export default {
         event.stopPropagation()
       }
       event.target.classList.add('was-validated')
-    }
-  }
+    },
+  },
 }
 </script>
 
