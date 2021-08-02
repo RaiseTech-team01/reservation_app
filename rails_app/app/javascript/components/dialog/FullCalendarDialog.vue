7b9aa1992d957eda967292643b72bad2f147f1b0
Yuki Nishimura <yukinkopen@gmail.com>
2021-08-03 04:55:41 +0900

    [mod] カレンダーの表示時に正常に表示されない問題を暫定的に対応   - 日付を移動すると表示が正常に行われる点を利用     - 初期値を前日にして、表示直後に１日進めて、結果的に当日を表示する形にする

--- rails_app/app/javascript/components/dialog/FullCalendarDialog.vue
+++ rails_app/app/javascript/components/dialog/FullCalendarDialog.vue
@@ -5,6 +5,7 @@
         <button @click.prevent.stop="show_timetable">ボタン2</button>
         <button @click.prevent.stop="show_timetable">ボタン2</button>
         <button @click.prevent.stop="show_timetable">ボタン2</button>
+        <button @click.prevent.stop="nextDayX">next</button>
         <div
             id="timetable-bg"
             class="hidden absolute w-full h-full z-10 bg-black opacity-50"
@@ -26,6 +27,7 @@
                     bg-white
                 "
                 :options="calendarOptions"
+                ref="fullCalendar"
                 v-show="isShowCalendar"
             />
         </div>
@@ -36,6 +38,7 @@
 import "@fullcalendar/core/vdom"; // solves problem with Vite
 import FullCalendar from "@fullcalendar/vue";
 import interactionPlugin from "@fullcalendar/interaction";
+import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 
 export default {
@@ -43,33 +46,45 @@ export default {
         return {
             isShowCalendar: false,
             calendarOptions: {
-                plugins: [interactionPlugin, timeGridPlugin],
+                plugins: [interactionPlugin, dayGridPlugin, timeGridPlugin],
                 initialView: "timeGridDay",
+                defaultView: "timeGridDay",
+                // TODO 初期表示時にカレンダーの表示が不十分な問題に対処（暫定対応としてnext()で表示が正常になる点を利用。ここで前日に設定）
+                initialDate: new Date().setDate(new Date().getDate() - 1),
+                render: this.renderX,
+                destroy: this.destroyX,
                 dateClick: this.handleDateClick,
                 eventClick: this.handleEventClick,
-                events: [
-                    {
-                        title: "event 1",
-                        date: "2021-08-02",
-                        start: "2021-08-02 16:30",
-                        end: "2021-08-02 18:30",
-                    },
-                    {
-                        title: "event 2",
-                        date: "2021-08-02",
-                        start: "2021-08-02 20:00",
-                        end: "2021-08-02 22:00",
-                    },
-                    {
-                        title: "event 3",
-                        date: "2021-08-02",
-                        start: "2021-08-02 20:00",
-                        end: "2021-08-02 22:00",
-                    },
-                ],
+                loading: this.loadingX,
+                ref: "fullCalendar",
+                // events: [
+                //     {
+                //         title: "event 1",
+                //         date: "2021-08-02",
+                //         start: "2021-08-02 16:30",
+                //         end: "2021-08-02 18:30",
+                //     },
+                //     {
+                //         title: "event 2",
+                //         date: "2021-08-02",
+                //         start: "2021-08-02 20:00",
+                //         end: "2021-08-02 22:00",
+                //     },
+                //     {
+                //         title: "event 3",
+                //         date: "2021-08-02",
+                //         start: "2021-08-02 20:00",
+                //         end: "2021-08-02 22:00",
+                //     },
+                // ],
                 slotDuration: "00:15",
                 slotMinTime: "16:00",
                 slotMaxTime: "24:00",
+                hiddenDays: [], // range:0-6, Sunday is 0
+                dayHeaders: false,
+                eventAfterRender: this.eventAfterRenderX,
+                success: this.successX,
+                eventAfterAllRender: this.eventAfterAllRenderX,
             },
         };
     },
@@ -77,8 +92,61 @@ export default {
         FullCalendar,
     },
     methods: {
+        eventAfterAllRenderX() {
+            console.log("eventAfterAllRender");
+        },
+        successX() {
+            console.log("success");
+        },
+        eventAfterRenderX() {
+            console.log("eventAfterRender");
+        },
+        loadingX(isLoading) {
+            console.log("loadingX", isLoading);
+        },
+        nextDayX() {
+            // this.$refs.fullCalendar.getApi();
+            console.log("api", this.$refs.fullCalendar.getApi());
+            console.log("api2", this.$refs.fullCalendar.getApi().__proto__);
+            console.log(
+                "api3",
+                this.$refs.fullCalendar.getApi().__proto__.__proto__
+            );
+            // console.log(
+            //     "api4",
+            //     this.$refs.fullCalendar
+            //         .getApi()
+            //         .__proto__.__proto__.getCurrentData()
+            // );
+            this.$refs.fullCalendar.getApi().next();
+        },
+        // getInitialDate() {
+        //     const dt = new Date().now();
+        //     console.log("now", dt);
+        //     dt.setDate(dt.getDate() + 1);
+        //     return dt;
+        // },
+        destroyX() {
+            console.log("destroyX");
+        },
+        renderX() {
+            console.log("render this", this);
+            // console.log("fc", FullCalendar);
+            // this.changeView("dayGridMonth");
+            // this.$refs.fullCalendar.getApi().changeView("timeGridDay");
+            // FullCalendar.getApi().changeView("dayGridMonth");
+            // this.$refs.fullCalendar.next();
+            this.$refs.fullCalendar.getApi().next();
+        },
+        batchRenderingX() {
+            console.log("batch");
+            this.changeView("dayGridMonth");
+            this.addEvent({ title: "new event", start: "2018-09-01" });
+        },
         dummy() {},
         handleDateClick: function (arg) {
+            console.log("this", this);
+            console.log(arg);
             alert("date click! " + arg.dateStr);
         },
         handleEventClick: function (arg) {
@@ -104,6 +172,11 @@ export default {
                 $("#timetable-dialog").show("normal", function () {
                     $("body, html").css({ overflow: "hidden", height: "100%" });
                 });
+                // TODO 初期表示時にカレンダーの表示が不十分な問題に対処（暫定対応としてnext()で表示が正常になる点を利用）
+                setTimeout(() => {
+                    console.log("setTimeout");
+                    this.$refs.fullCalendar.getApi().next();
+                }, 500);
             }
         },
     },
