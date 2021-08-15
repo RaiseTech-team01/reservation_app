12a5bf4fcf03a42f78dbb982071505da820898cc
KazusaNakagawa <kazusanakagawa@gmail.com>
2021-08-15 12:49:47 +0900

    fix: entrypoint.sh を読み込まないようにコメントアウト対応

--- rails_app/entrypoint.sh
+++ rails_app/entrypoint.sh
@@ -1,9 +1,10 @@
-#!/bin/ash
-set -e
+#!/bin/bash
+# set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
-rm -f /rails_app/tmp/pids/server.pid
+# rm -f /rails_app/tmp/pids/server.pid
+
 # rails s -b ${BACKEND_HOST}
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
-exec "$@"
+# exec "$@"
