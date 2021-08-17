3d5914f87eb90f22652c06f919af2b7766c67daa
Yuki Nishimura <yukinkopen@gmail.com>
2021-08-18 06:21:43 +0900

    [mod] ナビゲーションの表示を更新   - propsで現在のタブのindexを渡せるようにした   - 現在のタブは色を薄くして、選択不可にした

--- rails_app/app/javascript/components/layout/Navigation.vue
+++ rails_app/app/javascript/components/layout/Navigation.vue
@@ -3,7 +3,45 @@
         v-if="isLogin === true"
         class="h-16 text-center flex flex-row justify-center"
     >
-        <span
+        <template v-for="(nav, index) in navList">
+            <span
+                v-if="index === currentIndex"
+                class="
+                    flex flex-col
+                    justify-center
+                    w-1/6
+                    mr-1
+                    h-16
+                    text-2xl
+                    bg-red-400
+                    text-white
+                    cursor-pointer
+                "
+                :key="index"
+            >
+                {{ nav.title }}
+            </span>
+            <span
+                v-else
+                :key="index"
+                class="
+                    flex flex-col
+                    justify-center
+                    w-1/6
+                    mr-1
+                    h-16
+                    text-2xl
+                    bg-red-500
+                    hover:bg-red-700
+                    active:bg-yellow-700
+                    text-white
+                    cursor-pointer
+                "
+            >
+                {{ nav.title }}
+            </span>
+        </template>
+        <!-- <span
             class="
                 flex flex-col
                 justify-center
@@ -66,12 +104,11 @@
                 hover:bg-red-700
                 active:bg-yellow-700
                 text-white
-                font-bold
                 cursor-pointer
             "
             @click="goToAccountSettings"
             >アカウント設定</span
-        >
+        > -->
     </div>
 </template>
 
@@ -80,7 +117,29 @@ import Router from "../../router/router";
 
 export default {
     data: function () {
-        return {};
+        return {
+            navList: [
+                {
+                    title: "予約入力",
+                    callback: this.goToReservationForm,
+                },
+                {
+                    title: "予約一覧",
+                    callback: this.goToReservationList,
+                },
+                {
+                    title: "予約履歴",
+                    callback: this.goToReservationHistory,
+                },
+                {
+                    title: "アカウント設定2",
+                    callback: this.goToAccountSettings,
+                },
+            ],
+        };
+    },
+    props: {
+        currentIndex: number,
     },
     computed: {
         isLogin() {
