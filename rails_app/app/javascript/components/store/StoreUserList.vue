c34f876eb2ee49647f306e1642e15502c7d43fce
Nishimura <yukinkopen@gmail.com>
2021-08-19 22:44:59 +0900

    [RAISETECH-96] 管理_ユーザ一覧画面 (#141)
    * [mod] フォーマット処理を適用
    
    * [mod] リストに追加した値を画面に表示
    
    * [mod] セミコロンを除外する設定
    
    * [mod] <フォーマット> セミコロンを除外
    
    * [mod] <フォーマット> シングルクォーテーションを利用する形に修正
    
    * [mod] v-for でリストを表示
    
    * [mod] データの固定部分を除外
    
    * [mod] TODOを追加

--- rails_app/app/javascript/components/store/StoreUserList.vue
+++ rails_app/app/javascript/components/store/StoreUserList.vue
@@ -1,64 +1,50 @@
 <template>
-<div class="main m-0">
-  <dir class="storeheader m-0 text-center">
-    <StoreHeader />
-  </dir>
-  <div class="container-fluid mt-20">
-    <div class="row flex justify-center">
-      <main class="col-md-9">
-        <h2>ユーザ一覧画面</h2>
-        <div class="table-responsive">
-          <table class="table table-striped table-sm">
-            <thead>
-              <tr>
-                <th>#</th>
-                <th>ユーザ名</th>
-                <th>メールアドレス</th>
-                <th>年齢</th>
-                <th>性別</th>
-                <th>電話番号</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <td>1</td>
-                <td>田中一郎</td>
-                <td>user1@sample.com</td>
-                <td>30</td>
-                <td>男性</td>
-                <td>08011112222</td>
-              </tr>
-              <tr>
-                <td>2</td>
-                <td>佐藤二郎</td>
-                <td>user2@sample.com</td>
-                <td>35</td>
-                <td>男性</td>
-                <td>08033334444</td>
-              </tr>
-              <tr>
-                <td>3</td>
-                <td>川上三郎</td>
-                <td>user3@sample.com</td>
-                <td>40</td>
-                <td>男性</td>
-                <td>08055556666</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
-      </main>
+  <div class="main m-0">
+    <dir class="storeheader m-0 text-center">
+      <StoreHeader />
+    </dir>
+    <div class="container-fluid mt-20">
+      <div class="row flex justify-center">
+        <main class="col-md-9">
+          <h2>ユーザ一覧画面</h2>
+          <div class="table-responsive">
+            <table class="table table-striped table-sm">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>ユーザ名</th>
+                  <th>メールアドレス</th>
+                  <th>年齢</th>
+                  <th>性別</th>
+                  <th>電話番号</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr v-for="user in userList" :key="user.id">
+                  <td>{{ user.id }}</td>
+                  <td>{{ user.name }}</td>
+                  <td>{{ user.email }}</td>
+                  <td>{{ user.age }}</td>
+                  <td>{{ user.gendar }}</td>
+                  <td>{{ user.tel }}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </main>
+      </div>
     </div>
   </div>
-</div>
 </template>
 
 <script>
-import StoreHeader from "../layout/StoreHeader.vue"
+import StoreHeader from '../layout/StoreHeader.vue'
+import axios from 'axios'
 
 export default {
   data: function () {
     return {
+      userList: [],
     }
   },
 
@@ -73,8 +59,83 @@ export default {
         event.stopPropagation()
       }
       event.target.classList.add('was-validated')
-    }
-  }
+    },
+    initialize() {
+      console.log('init')
+    },
+    addData(data) {
+      this.userList.push(data)
+    },
+    async submit(successCallback) {
+      this.typedEmail = document.getElementById('user_email').value
+      this.typedPassword = document.getElementById('user_pass').value
+      this.loading = true
+      const params = {
+        email: `${this.typedEmail}`,
+        password: `${this.typedPassword}`,
+      }
+      await axios
+        .post('/api/v1/auth/sign_in', params)
+        .then((response) => {
+          // TODO ユーザ一覧データを取得するためのAPIを叩き、取得したデータをuserListに設定する
+
+          successCallback()
+          // localStorage.setItem("access-token", response.headers["access-token"])
+          // localStorage.setItem("uid", response.headers["uid"])
+          // localStorage.setItem("client", response.headers["client"])
+
+          // // console.log(response.data.data)
+          // this.loginedUserData = response.data.data
+          // console.log(this.loginedUserData)
+
+          // // Vuex store
+          // this.$store.dispatch('userData/update', this.loginedUserData)
+          // this.$store.dispatch('auth/updateLogin', true)
+          // Router.push("/api/v1/user/account_info")
+        })
+        .catch((error) => {
+          // TODO エラー情報をアラートとして画面に表示する
+          // TODO: 適切な Error 表示
+          // console.log(error.response),
+          // console.log(error.response.data.errors),
+          // this.$store.dispatch('userData/updateErr', error.response.data.errors)
+          // Router.push("/login")
+        })
+        .finally(() => {
+          this.loading = false
+        })
+      console.log('axios finished')
+    },
+  },
+  mounted: function () {
+    // submit(() => {
+    //   this.initialize();
+    // })
+    this.addData({
+      id: '1',
+      name: '木村雄二',
+      email: 'user1@sample.com',
+      age: 25,
+      gendar: '男性',
+      tel: '08034566543',
+    })
+    this.addData({
+      id: '2',
+      name: '田中紀子',
+      email: 'user2@sample.com',
+      age: 30,
+      gendar: '女性',
+      tel: '08098761234',
+    })
+    this.addData({
+      id: '3',
+      name: '村上庄司',
+      email: 'user3@sample.com',
+      age: 20,
+      gendar: '男性',
+      tel: '08098761234',
+    })
+  },
 }
 </script>
 
