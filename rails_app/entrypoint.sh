10779e9b4b00e13f951075ae68596af2f94e59b2
KazusaNakagawa <kazusanakagawa@gmail.com>
2021-08-22 18:15:59 +0900

    fix: ファイル調整

--- rails_app/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/ash
-set -e
-
-# Remove a potentially pre-existing server.pid for Rails.
-rm -f /rails_app/tmp/pids/server.pid
-# rails s -b ${BACKEND_HOST}
-
-# Then exec the container's main process (what's set as CMD in the Dockerfile).
-exec "$@"
