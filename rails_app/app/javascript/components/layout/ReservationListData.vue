a3b2157d546feb9cf21d73288d4b46d3c75d0f95
Yuki Nishimura <yukinkopen@gmail.com>
2021-08-26 10:29:25 +0900

    [mod] 画面に合わせてリストの表示条件を変更   - 予約一覧：現在時刻が予約日時以前の場合にリストに表示   - 予約履歴：現在時刻が予約日時より後の場合にリストに表示

--- rails_app/app/javascript/components/layout/ReservationListData.vue
+++ rails_app/app/javascript/components/layout/ReservationListData.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
     <div
-      v-for="(item, arryIndex) in userReservationData.reservationDataArray"
+      v-for="(
+        item, arryIndex
+      ) in userReservationData.reservationDataArray.filter((d) =>
+        isShowItem(d.date_at)
+      )"
       :key="arryIndex"
     >
       <table
@@ -92,6 +96,7 @@ export default {
   data: function () {
     return {
       arryIndex: "",
+      reservListType,
     }
   },
   props: {
@@ -112,37 +117,55 @@ export default {
         client: localStorage.getItem("client"),
       },
     }
-    axios
-      .get(
-        //  atode
-        //  API変更にてコメントアウト
-        // `http://localhost:3000/api/v1/user/${this.userData.id}/reservations/`,key_headers)
-        `http://localhost:3000/api/v1/user/reservations/`,
-        key_headers
-      )
-      .then((response) => {
-        console.log(response.data)
-        console.log(response.data[0])
-        console.log(response.data[0].user)
-        // Vuex store
-        this.$store.dispatch("userReservationData/update", response.data)
-        this.$store.dispatch("userReservationData/updateErr", "")
-      })
-
-      .catch((error) => {
-        // TODO: 適切な Error 表示
-        console.log(error.response)
-        console.log(error.response.data.error)
-        this.$store.dispatch(
-          "userReservationData/updateErr",
-          error.response.data.error
-        )
-      })
-      .finally(() => {
-        console.log("axios finished")
-      })
+    this.requestReservationList(key_headers)
   },
   methods: {
+    requestReservationList(key_headers) {
+      axios
+        .get(
+          //  atode
+          //  API変更にてコメントアウト
+          // `http://localhost:3000/api/v1/user/${this.userData.id}/reservations/`,key_headers)
+          `http://localhost:3000/api/v1/user/reservations/`,
+          key_headers
+        )
+        .then((response) => {
+          console.log(response.data)
+          console.log(response.data[0])
+          console.log(response.data[0].user)
+          this.$store.dispatch("userReservationData/update", response.data)
+          this.$store.dispatch("userReservationData/updateErr", "")
+        })
+
+        .catch((error) => {
+          // TODO: 適切な Error 表示
+          console.log(error.response)
+          console.log(error.response.data.error)
+          this.$store.dispatch(
+            "userReservationData/updateErr",
+            error.response.data.error
+          )
+        })
+        .finally(() => {
+          console.log("axios finished")
+        })
+    },
+    isShowItem(date_at) {
+      switch (this.type) {
+        case this.reservListType.reservList:
+          return !this.isDateExpired(date_at)
+        case this.reservListType.historyList:
+          return this.isDateExpired(date_at)
+      }
+    },
+    // 予約データが期限切れか否かを判定
+    isDateExpired(date_at, date_on) {
+      // TODO サーバから date_at: 日付、date_on: 時間 で返される対応がされたら修正する
+      // TODO 現状は date_at : "2021-08-26T19:15:00.000+09:00" を過ぎたら期限切れと判定する
+      const expiredDateLong = Date.parse(date_at)
+      const nowDateLong = Date.now()
+      return nowDateLong > expiredDateLong
+    },
     goToDetail(arryIndex) {
       console.log(`${arryIndex}`)
       // this.selected_rdId = counter
